6b5de7d5272b7c3cda7fd395677d1a8777de7ff0
ChocOZerO <nayalth@gmail.com>
2017-12-19 21:52:10 +0900

    [Update]First Label #6

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -16,6 +16,8 @@ class FirstViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print(#file, #line, #function, #column)
         self.firstLabel.text = "Jake의 사진액자"
+        self.firstLabel.textColor = UIColor.init(red: 1.0, green: 0.7, blue: 0.5, alpha: 0.6)
+        self.firstLabel.backgroundColor = UIColor.gray
     }
 
     override func didReceiveMemoryWarning() {
