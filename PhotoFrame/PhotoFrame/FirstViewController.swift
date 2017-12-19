a60417cdc2d7382efd7d115c07864ac37c8f7510
ChocOZerO <nayalth@gmail.com>
2017-12-19 17:00:56 +0900

    [Add]photoframe project #1

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by TaeHyeonLee on 2017. 12. 19..
+//  Copyright © 2017년 ChocOZerO. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
