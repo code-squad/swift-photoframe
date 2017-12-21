ec5585e8494d5ce30ed7d6c60e5ebad3629d5924
ChocOZerO <nayalth@gmail.com>
2017-12-21 16:46:51 +0900

    [Update]First2DepthViewController로 닫기 기능 추가 #17

---
+++ PhotoFrame/PhotoFrame/First2DepthViewController.swift
@@ -0,0 +1,38 @@
+//
+//  First2DepthViewController.swift
+//  PhotoFrame
+//
+//  Created by TaeHyeonLee on 2017. 12. 21..
+//  Copyright © 2017년 ChocOZerO. All rights reserved.
+//
+
+import UIKit
+
+class First2DepthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
