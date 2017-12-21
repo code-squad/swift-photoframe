738be4d61fb435451fcc8026709bbee888d2dedd
DongJin Lee <comets195@gmail.com>
2017-12-21 11:40:57 +0900

    Add Project and Change Readme file..#1

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by Mrlee on 2017. 12. 21..
+//  Copyright © 2017년 Napster. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
