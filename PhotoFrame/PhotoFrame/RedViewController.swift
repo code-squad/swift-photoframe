b9f6156d2a69fa79c30993b94b59419b05db1cef
DongJin Lee <comets195@gmail.com>
2017-12-22 11:34:48 +0900

    Add when do not use segue changing view.. #6

--- PhotoFrame/PhotoFrame/RedViewController.swift
+++ PhotoFrame/PhotoFrame/RedViewController.swift
@@ -9,13 +9,30 @@
 import UIKit
 
 class RedViewController: UIViewController {
+    @IBAction func nextSceneButton(_ sender: Any) {
+        let storyBoard = self.storyboard!
+        let nextView = storyBoard.instantiateViewController(withIdentifier: "greenView")
+        present(nextView, animated: true, completion: nil)
+    }
     @IBAction func closeButtonTouched(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(#file, #line, #function, #column)
         // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
