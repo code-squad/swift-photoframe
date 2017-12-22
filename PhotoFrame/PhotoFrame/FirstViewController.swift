daac2b8456e5687ac83cf94f7ae5f6288fb9afb9
DongJin Lee <comets195@gmail.com>
2017-12-22 11:34:48 +0900

    Add when do not use segue changing view.. #6

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -9,23 +9,23 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-    @IBOutlet weak var photoLable: UILabel!
+    @IBOutlet weak var photoLabel: UILabel!
     @IBOutlet weak var firstDescription: UILabel!
     @IBAction func touchDown(_ sender: Any) {
-        self.photoLable.font = UIFont.systemFont(ofSize: 50)
-        self.photoLable.textColor = UIColor.brown
-        self.photoLable.backgroundColor = UIColor.white
+        self.photoLabel.font = UIFont.systemFont(ofSize: 50)
+        self.photoLabel.textColor = UIColor.brown
+        self.photoLabel.backgroundColor = UIColor.white
     }
     @IBAction func nextButtonTouched(_ sender: Any) {
-        self.photoLable.textColor = UIColor.blue
-        self.photoLable.backgroundColor = UIColor.yellow
-        self.photoLable.alpha = 0.5
+        self.photoLabel.textColor = UIColor.blue
+        self.photoLabel.backgroundColor = UIColor.yellow
+        self.photoLabel.alpha = 0.5
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.photoLable.text = "Napster"
-        self.photoLable.font = UIFont.systemFont(ofSize: 50)
-        self.photoLable.textColor = UIColor.brown
+        self.photoLabel.text = "Napster"
+        self.photoLabel.font = UIFont.systemFont(ofSize: 50)
+        self.photoLabel.textColor = UIColor.brown
         self.firstDescription.text = "Napster의 사진액자 입니다. 반갑습니다."
         // swiftlint:disable line_length
         self.firstDescription.textColor = UIColor(red: CGFloat(255), green: CGFloat(255), blue: CGFloat(102), alpha: CGFloat(20))
