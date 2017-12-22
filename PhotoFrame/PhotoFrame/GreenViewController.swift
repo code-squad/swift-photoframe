d2c42284d4be49f9357b5a500cd05115ce7431eb
DongJin Lee <comets195@gmail.com>
2017-12-22 10:25:26 +0900

    Add GreenViewController connected viewController in storyBoard.

---
+++ PhotoFrame/PhotoFrame/GreenViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GreenViewController.swift
+//  PhotoFrame
+//
+//  Created by Mrlee on 2017. 12. 22..
+//  Copyright © 2017년 Napster. All rights reserved.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
