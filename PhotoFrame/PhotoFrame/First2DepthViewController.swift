2c3b66ad3851cd69163bafb273c4fd726e138474
ChocOZerO <nayalth@gmail.com>
2017-12-22 16:51:11 +0900

    [Update]close button for navigation bar #21

--- PhotoFrame/PhotoFrame/First2DepthViewController.swift
+++ PhotoFrame/PhotoFrame/First2DepthViewController.swift
@@ -23,7 +23,7 @@ class First2DepthViewController: UIViewController {
     
 
     @IBAction func closeButtonTouched(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+        self.navigationController?.popViewController(animated: true)
     }
 
     override func viewWillAppear(_ animated: Bool) {
