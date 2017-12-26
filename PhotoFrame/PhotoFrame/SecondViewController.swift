cab8716ee4848a728623e0c7b878a3776f8affb7
DongJin Lee <comets195@gmail.com>
2017-12-26 20:53:59 +0900

    Add UIImagePickerController...

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -18,6 +18,14 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func selectButtonTouched(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imgPicker = UIImagePickerController()
+            imgPicker.delegate = self
+            imgPicker.sourceType = .photoLibrary
+            self.present(imgPicker, animated: true, completion: nil)
+        }
+    }
     @IBAction func nextImageButtonTouched(_ sender: Any) {
         let randomNumber = makeRandom()
         self.photoImageView.image = UIImage(named: String(format: "%02d.jpg", randomNumber))
@@ -26,5 +34,15 @@ class SecondViewController: UIViewController {
         let MAX: UInt32 = 22
         let MIN: UInt32 = 1
         return Int(arc4random_uniform(MAX) + MIN)
+    }
+}
+
+extension SecondViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+        if let pickedImg = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.photoImageView.contentMode = .scaleAspectFit
+            self.photoImageView.image = pickedImg
+        }
+        dismiss(animated: true, completion: nil)
     }
 }
