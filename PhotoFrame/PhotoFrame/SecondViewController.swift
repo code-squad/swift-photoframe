d7ae44d54165bb0a0425ede0108f2d07dc2031a6
DongJin Lee <comets195@gmail.com>
2017-12-26 11:09:32 +0900

    Add UIImageView class to show Images..

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -9,15 +9,29 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    @IBOutlet weak var photoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+        let imgExtension = ".jpg"
+        var imgName = ""
+        let randomNumber = makeRandom()
+        if randomNumber < 10 {
+            imgName = "0" + String(randomNumber)
+        } else {
+            imgName = String(randomNumber)
+        }
+        self.photoImageView.image = UIImage(named: imgName + imgExtension)
+    }
+    private func makeRandom() -> Int {
+        let MAX: UInt32 = 22
+        let MIN: UInt32 = 1
+        return Int(arc4random_uniform(MAX) + MIN)
+    }
 }
