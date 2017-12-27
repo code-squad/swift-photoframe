1a0b317be918971d0406d559a353c3b9a718a89b
undervine <undervine@naver.com>
2017-12-27 17:11:21 +0900

    Create PhotoFrame tapped app

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by 심 승민 on 2017. 12. 27..
+//  Copyright © 2017년 심 승민. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
