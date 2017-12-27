3fc0053852c77daced39b56f7b72c1d7f497a786
undervine <undervine@naver.com>
2017-12-27 17:12:09 +0900

    Add contents about UITabBar and UITabBarController

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -12,7 +12,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(#file, #line, #function, #column)
     }
 
     override func didReceiveMemoryWarning() {
