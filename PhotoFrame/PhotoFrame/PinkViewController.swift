55b5154732eeb7d17e13bb55572e6f172ffed0b7
undervine <undervine@naver.com>
2018-01-02 20:02:40 +0900

    Add 'Back' buttons to the Scenes
    Link 'Back' buttons to IBAction method 'closeButtonTouched()' of each view controllers, unwinding the screens.

---
+++ PhotoFrame/PhotoFrame/PinkViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PinkViewController.swift
+//  PhotoFrame
+//
+//  Created by 심 승민 on 2018. 1. 2..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import UIKit
+
+class PinkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+
+    @IBAction func closeButtonTouched(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+}
