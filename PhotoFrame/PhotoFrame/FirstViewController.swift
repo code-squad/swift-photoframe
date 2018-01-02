09d71d763f0e3d6e23534295b8ee93d74b10ce31
undervine <undervine@naver.com>
2018-01-02 14:23:25 +0900

    Add two view controllers and link them by segue

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -17,32 +17,39 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         // 속성이 지정된 문자열을 라벨에 적용
         let attributedString = NSMutableAttributedString(string: FrameInfo.title)
-        makeDefaultText(attributedString)
+        let defaultFont = modifyFont(attributedString, FrameInfo.nameRange,
+                                     FrameInfo.fontName, CGFloat(FrameInfo.fontSize))
+        modifyFontSize(attributedString, FrameInfo.nameRange, defaultFont, to: 33.0)
+//        makeGrayTitle(attributedString)
+        makeYellowTitle(attributedString)
+        photoLabel.attributedText = attributedString
         // 부제목 텍스트 속성 변경
         firstDescription.text = FrameInfo.subtitle
         firstDescription.textColor = UIColor.darkGray
     }
 
-    @IBAction func nextButtonTouched(_ sender: UIButton) {
+    @IBAction func nextButtonClicked(_ sender: UIButton) {
         // 기존 텍스트의 속성 받아옴.
         guard let photoLabelAttributeText = photoLabel.attributedText else { return }
         let attributedString = NSMutableAttributedString(attributedString: photoLabelAttributeText)
-        // 제목 텍스트 생성 및 폰트 적용
-        modifyBackgroundColor(attributedString, FrameInfo.nameRange, to: UIColor.yellow)
-        modifyTextColor(attributedString, FrameInfo.restRange, to: UIColor.yellow)
+        makeYellowTitle(attributedString)
         photoLabel.attributedText = attributedString
     }
 
 }
 
 extension FirstViewController {
-    private func makeDefaultText(_ attributedString: NSMutableAttributedString) {
-        let defaultFont = modifyFont(attributedString, FrameInfo.nameRange, FrameInfo.fontName, CGFloat(FrameInfo.fontSize))
-        modifyFontSize(attributedString, FrameInfo.nameRange, defaultFont, to: 33.0)
-        modifyBackgroundColor(attributedString, FrameInfo.nameRange, to: UIColor.gray)
+    private func makeGrayTitle(_ attributedString: NSMutableAttributedString) {
         modifyTextColor(attributedString, FrameInfo.nameRange, to: UIColor.white)
-        modifyForegroundColor(attributedString, FrameInfo.restRange, to: UIColor.gray)
-        photoLabel.attributedText = attributedString
+        modifyTextColor(attributedString, FrameInfo.restRange, to: UIColor.gray)
+        modifyBackgroundColor(attributedString, FrameInfo.nameRange, to: UIColor.gray)
+    }
+
+    private func makeYellowTitle(_ attributedString: NSMutableAttributedString) {
+        // 제목 텍스트 생성 및 폰트 적용
+        modifyTextColor(attributedString, FrameInfo.nameRange, to: UIColor.white)
+        modifyTextColor(attributedString, FrameInfo.restRange, to: UIColor.yellow)
+        modifyBackgroundColor(attributedString, FrameInfo.nameRange, to: UIColor.yellow)
     }
 
     private func modifyFont(_ attributedString: NSMutableAttributedString, _ range: NSRange,
@@ -56,25 +63,19 @@ extension FirstViewController {
     private func modifyFontSize(_ attributedString: NSMutableAttributedString, _ range: NSRange,
                                 _ font: UIFont?, to size: CGFloat) {
         // 제목 텍스트 일부분의 크기 조절
-        let sizeAttribute = [NSAttributedStringKey.font: font?.withSize(size)]
-        attributedString.addAttributes(sizeAttribute, range: range)
+        let attribute = [NSAttributedStringKey.font: font?.withSize(size)]
+        attributedString.addAttributes(attribute, range: range)
     }
 
     // 제목 텍스트 일부분의 배경색상 적용
     private func modifyBackgroundColor(_ attributedString: NSMutableAttributedString, _ range: NSRange,
                                        to color: UIColor) {
-        let backgroundAttribute = [NSAttributedStringKey.backgroundColor: color]
-        attributedString.addAttributes(backgroundAttribute, range: range)
-    }
-
-    private func modifyForegroundColor(_ attributedString: NSMutableAttributedString, _ range: NSRange,
-                                       to color: UIColor) {
-        let foregroundAttribute = [NSAttributedStringKey.foregroundColor: color]
-        attributedString.addAttributes(foregroundAttribute, range: range)
+        let attribute = [NSAttributedStringKey.backgroundColor: color]
+        attributedString.addAttributes(attribute, range: range)
     }
 
     private func modifyTextColor(_ attributedString: NSMutableAttributedString, _ range: NSRange, to color: UIColor) {
-        let backgroundAttribute = [NSAttributedStringKey.foregroundColor: color]
-        attributedString.addAttributes(backgroundAttribute, range: range)
+        let attribute = [NSAttributedStringKey.foregroundColor: color]
+        attributedString.addAttributes(attribute, range: range)
     }
 }
