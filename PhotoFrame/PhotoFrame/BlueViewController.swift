8a89e410880d24bfd0463196815d356c1adb8d26
undervine <undervine@naver.com>
2018-01-02 20:17:15 +0900

    Remove segue and add codes for screen transition

--- PhotoFrame/PhotoFrame/BlueViewController.swift
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -15,6 +15,12 @@ class BlueViewController: UIViewController {
         print(#file, #line, #function, #column)
     }
 
+    // 세그 없이 다음 화면 이동.
+    @IBAction func nextButtonTouched(_ sender: UIButton) {
+        if let pvc = self.storyboard?.instantiateViewController(withIdentifier: "PVC") {
+            self.present(pvc, animated: true, completion: nil)
+        }
+    }
     @IBAction func closeButtonTouched(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
         //self.presentingViewController?.dismiss(animated: true, completion: nil)
