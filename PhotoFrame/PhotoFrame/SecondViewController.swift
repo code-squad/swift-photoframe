70124ce71e6851d8aca63d8b8fc9836e7e8b412e
undervine <undervine@naver.com>
2018-01-04 18:40:20 +0900

    Make SecondViewController to show random images

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -10,14 +10,48 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    private var images: [UIImage]
+
+    required init?(coder aDecoder: NSCoder) {
+        self.images = []
+        super.init(coder: aDecoder)
+        guard let images = generateUIImages(getJPGImageFileNames()) else { return nil }
+        self.images = images
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(#file, #line, #function, #column)
+        NSLog(#file, #line, #function, #column)
+        self.photoImageView.image = getRandomImage()
+        self.photoImageView.contentMode = .scaleAspectFill
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func nextImageButtonTouched(_ sender: UIButton) {
+        if let randomImage = getRandomImage() {
+            self.photoImageView.image = randomImage
+        }
     }
 
+    private func generateUIImages(_ directoryAndFileNames: [String]?) -> [UIImage]? {
+        guard let fileNames = directoryAndFileNames else { return nil }
+        let uiImages = fileNames.flatMap { UIImage(named: $0) }
+        return uiImages
+    }
+
+    private func getJPGImageFileNames() -> [String]? {
+        guard let path = Bundle.main.resourcePath else { return nil }
+        let fileManager = FileManager.default
+        var jpgFileNames: [String]?
+        if let fileNames = try? fileManager.contentsOfDirectory(atPath: path) {
+            // 폴더명은 따로 붙여줄 필요 없음.
+            jpgFileNames = fileNames.filter({ $0.hasSuffix(".jpg") })
+        }
+        return jpgFileNames
+    }
+
+    private func getRandomImage() -> UIImage? {
+        let randomIndex = Int(arc4random_uniform(UInt32(self.images.count)))
+        return self.images[randomIndex]
+    }
 }
