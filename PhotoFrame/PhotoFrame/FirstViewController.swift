babbf17c77263ac8d29e7cff1ada14564554d1db
YUAMING <miennes3@gmail.com>
2018-01-29 18:14:27 +0900

    2단계 생성

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    @IBOutlet weak var photoLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
+        
+        self.photoLabel.text = "AMING의 사진액자"
+        self.photoLabel.textColor = UIColor(red: 0.333, green: 0.455, blue: 0.116, alpha: 1.0)
+        self.photoLabel.font = UIFont.boldSystemFont(ofSize: 40)
+        self.firstDescription.text = "AMING의 사진액자입니다 :)"
+        self.firstDescription.textColor = UIColor.green
     }
 
     override func didReceiveMemoryWarning() {
