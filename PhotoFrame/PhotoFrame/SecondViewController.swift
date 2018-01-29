1526187efdde7aaa9351aa71b3f1fe3cbb487219
YUAMING <miennes3@gmail.com>
2018-01-29 16:49:19 +0900

    1단계 생성
    * README.adoc 작성
    * README.adoc 필요한 이미지 추가

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by yuaming on 2018. 1. 29..
+//  Copyright © 2018년 yuaming. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
