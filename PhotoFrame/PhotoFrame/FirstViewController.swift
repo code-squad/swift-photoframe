22f753fa16b0d159e86ff1c335f3dc8a40b5455c
dely2p <lhyal2p@gmail.com>
2018-01-30 18:08:58 +0900

    create Tabbed App templat #1
    Tabbed App 템플릿을 만들고 viewDidLoad() 메소드에 프로젝트파일 정보를 print를 하도록 만들었다.

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by Eunjin Kim on 2018. 1. 30..
+//  Copyright © 2018년 Eunjin Kim. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
