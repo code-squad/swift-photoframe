ab28afb7ab0215cc8a58d61b89f94092c7e9a11e
Jack <somedd@naver.com>
2018-01-30 17:50:39 +0900

    Edit Second Scene for adding Photo Album. #13

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+        let randomPhotoNumber = arc4random_uniform(22) + 1
+        if randomPhotoNumber < 10 {
+                    self.photoImageView.image = UIImage(named: "0\(randomPhotoNumber).jpg")
+            return
+        }
+        self.photoImageView.image = UIImage(named: "\(randomPhotoNumber).jpg")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
