23dd9cd3ff19506c29f2d55539a886568680f9db
dely2p <lhyal2p@gmail.com>
2018-03-05 13:25:19 +0900

    connect the first label to IBOutlet #4
    first Label과 firstDescription Label을 IBOutlet으로 연결하고 text 컬러, 크기, 배경색을 변경했다.

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.firstLabel.text = "Elly의 사진액자"
+        self.firstDescription.text = "글자색, 배경색, 투명도, 글자 크기를 바꿔본 Description"
+        self.firstDescription.textColor = UIColor.white
+        self.firstDescription.backgroundColor = UIColor.blue.withAlphaComponent(0.5)
+        self.firstDescription.font = UIFont.boldSystemFont(ofSize: 15)
         print(#file, #line, #function, #column)
     }
 
