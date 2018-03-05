f8d4e29d4f3eb90e965802dac01fc2bd20957bbc
dely2p <lhyal2p@gmail.com>
2018-03-05 17:55:56 +0900

    add closeButtonTouched() method in YellowViewController #11
    YellowViewController에 닫기 버튼 메소드를 생성하고 구현했다.

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -12,24 +12,13 @@ class YellowViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
 }
