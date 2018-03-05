99af2f0502ba430410eb65533d6fae9a32d07acc
dely2p <lhyal2p@gmail.com>
2018-03-05 17:37:48 +0900

    add ViewController of new scenes #11
    새로 추가한 scene 두 개에 각각 연결할 ViewController를 생성했다.

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,35 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by Eunjin Kim on 2018. 3. 5..
+//  Copyright © 2018년 Eunjin Kim. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
