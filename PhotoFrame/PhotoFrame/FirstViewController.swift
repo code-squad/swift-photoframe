405d6afd8999e83dd81b1e34ede3086fba57cc9f
dely2p <lhyal2p@gmail.com>
2018-03-08 15:36:04 +0900

    fix view as iPhoneSE to test at iphone #23
    폰에 올려서 테스트 해보니 사이즈가 맞지 않아 iPhoneSE로 수정(테스트 디바이스는 iphone6s이나 iphone8으로 구성하니 안맞는 것 같다.)

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -15,7 +15,7 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.firstLabel.text = "Elly의 사진액자"
-        self.firstDescription.text = "글자색, 배경색, 투명도, 글자 크기를 바꿔본 Description"
+        self.firstDescription.text = "글자색, 배경색, 투명도, 글자 크기를 바꿈"
         self.firstDescription.textColor = UIColor.white
         self.firstDescription.backgroundColor = UIColor.blue.withAlphaComponent(0.5)
         self.firstDescription.font = UIFont.boldSystemFont(ofSize: 15)
