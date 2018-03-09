447994285d24983625fd31cf8cebf4fbaf9804c1
Mijin Jeon <mayihelpu27@gmail.com>
2018-03-09 16:53:58 +0900

    Implement step1

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by YOUTH on 2018. 3. 9..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+
+    override func didReceiveMemoryWarning() {
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by YOUTH on 2018. 3. 9..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
