4e2886d5b2fbbf7461610c27ca3e6bed5fbec4c2
Mijin Jeon <mayihelpu27@gmail.com>
2018-03-12 15:12:00 +0900

    Implement step3

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -12,6 +12,9 @@ class FirstViewController: UIViewController {
 
     @IBOutlet weak var photoLabel: UILabel!
     @IBOutlet weak var firstDescription: UILabel!
+
+    @IBOutlet weak var nextButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.photoLabel.text = "알린의 사진액자"
@@ -20,6 +23,7 @@ class FirstViewController: UIViewController {
         self.firstDescription.text = "사진액자 firstDescription"
         let colorLiteral = #colorLiteral(red: 0.1380051349, green: 0.3703911386, blue: 1, alpha: 0.7430436644)
         self.firstDescription.textColor = colorLiteral
+        self.nextButton.backgroundColor = UIColor.green
         print(#file, #line, #function, #column)
     }
 
@@ -27,6 +31,13 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextButtonTouched(_ sender: Any) {
+        self.photoLabel.textColor = UIColor.blue
+        self.photoLabel.backgroundColor = UIColor.gray
+        self.photoLabel.alpha = 0.5
+        self.nextButton.backgroundColor = UIColor.gray
+        self.nextButton.alpha = 0.5
+    }
 
 }
 
