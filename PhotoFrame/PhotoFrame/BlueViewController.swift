d62536bb6f20e55fd53f6210e55ed0c10f40f687
Mijin Jeon <mayihelpu27@gmail.com>
2018-03-13 19:48:40 +0900

    Add BlueViewController class

---
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BlueViewController.swift
+//  PhotoFrame
+//
+//  Created by YOUTH on 2018. 3. 13..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
