6ecdf0c64842ad547e71ae276d3816862d243f2b
Mijin Jeon <mayihelpu27@gmail.com>
2018-03-19 15:58:43 +0900

    i UIImagePickerControllerDelegate

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var photoImageView: UIImageView!
 
     override func viewDidLoad() {
@@ -27,8 +27,34 @@ class SecondViewController: UIViewController {
     }
 
     @IBAction func nextImageButtonTouched(_ sender: Any) {
-        self.photoImageView.image = UIImage(named: "DemoImages/\(self.generateRandomImageNumber()).jpg")
+        self.photoImageView.image = UIImage(named: "\(self.generateRandomImageNumber()).jpg")
     }
     
+    @IBAction func selectButtonTouched(_ sender: UIButton) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.delegate = self
+            self.present(imagePicker, animated: true, completion: nil)
+    }
+
+    // 이미지 피커에서 이미지 선택 안했을 때
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: false, completion: {() in
+            let alert = UIAlertController(title: "", message: "이미지 선택 취소", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+            self.present(alert, animated: false)
+        })
+
+    }
+
+    // 이미지 피커에서 이미지 선택 했을 때
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        self.dismiss(animated: false) {() in
+            let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+            self.photoImageView.image = selectedImage
+        } // completion 파라미터 부분 트레일링 클로저 사용
+    }
 }
+
 
