7af9637069a5c1f8f1f15c0ed3583ef75b59355d
cocomilktea <coco_milktea@naver.com>
2018-05-14 11:24:21 +0900

    README에  UITabBarController 와 UITabBar 정리

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by 김수현 on 2018. 5. 11..
+//  Copyright © 2018년 김수현. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
