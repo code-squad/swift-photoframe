75ff993e5da49d4bacd7dde25109ee68f16fc962
Jung Kim <godrm77@gmail.com>
2018-05-16 16:40:20 +0900

    Revert "firstLabel IBOutlet 연결 및 속성 변경"
    This reverts commit cdee749df86de629d0b99b75cea72daaf0b0a690.

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -10,20 +10,8 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
-    @IBOutlet var firstView: UIView!
-    @IBOutlet weak var firstLabel: UILabel!
-    @IBOutlet weak var firstDescription: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.firstView.backgroundColor = UIColor(red:0.58, green:0.76, blue:0.93, alpha:1.0)
-        self.firstLabel.text = "hyun의 사진액자"
-        self.firstLabel.textColor = UIColor(red:0.04, green:0.44, blue:0.62, alpha:1.0)
-        self.firstLabel.backgroundColor = UIColor.clear
-        self.firstLabel.font = UIFont.systemFont(ofSize: 50)
-        self.firstDescription.text = "photo frame 🌃"
-        self.firstDescription.textColor = UIColor.darkGray
-        
         print(#file, #line, #function, #column)
         // Do any additional setup after loading the view, typically from a nib.
     }
