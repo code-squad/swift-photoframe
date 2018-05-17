29d62ea210f952b81dd442fa0e87b8fa7b8a0d63
cocomilktea <coco_milktea@naver.com>
2018-05-17 16:20:46 +0900

    새로운 ViewController 추가

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,60 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by 김수현 on 2018. 5. 16..
+//  Copyright © 2018년 김수현. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func nextButtonTouched(_ sender: Any) {
+        guard let pinkvc = self.storyboard?.instantiateViewController(withIdentifier: "PinkVC") else { return }
+        pinkvc.modalTransitionStyle = UIModalTransitionStyle.coverVertical
+        self.present(pinkvc, animated: true, completion: nil)
+    }
+    
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
