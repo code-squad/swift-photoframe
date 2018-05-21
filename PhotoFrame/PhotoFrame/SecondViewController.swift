f4e17e3857c64c177505583547d036a458a6ad25
cocomilktea <coco_milktea@naver.com>
2018-05-21 14:04:02 +0900

    photoframe 이미지 추가

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -22,13 +22,11 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-//    파일명을 결정할 때 String(format:)을 쓰는 방법으로 if-else 를 쓰지말고 개선해보세요.
     
     @IBAction func nextImageButtonTouched(_ sender: Any) {
         let randomNumber: UInt32 = arc4random_uniform(22)+1
-        let imageName = String(format: "%02d", randomNumber)
-        self.photoImageView.image = UIImage(named: "\(imageName).jpg")
+        let imageName = String(format: "%02d.jpg", randomNumber)
+        self.photoImageView.image = UIImage(named: imageName)
     }
     
 }
