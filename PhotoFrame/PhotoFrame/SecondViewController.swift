c56ef9cac65c44784e3ad3e626aa8d03a8dcd833
cocomilktea <coco_milktea@naver.com>
2018-05-21 17:20:42 +0900

    카메라 기능 구현

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var photoImageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    var captureImage: UIImage!
+    var imageSave = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         print(#file, #line, #function, #column)
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -29,5 +34,34 @@ class SecondViewController: UIViewController {
         self.photoImageView.image = UIImage(named: imageName)
     }
     
+    @IBAction func selectButtonTouched(_ sender: Any) {
+        let alert = UIAlertController()
+        let library =  UIAlertAction(title: "사진앨범", style: .default) { (action) in self.openLibrary() }
+        let camera =  UIAlertAction(title: "카메라", style: .default) { (action) in self.openCamera() }
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func openLibrary(){
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: false, completion: nil)
+    }
+    
+    func openCamera(){
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: false, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            photoImageView.image = image
+            print(info)
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
 
