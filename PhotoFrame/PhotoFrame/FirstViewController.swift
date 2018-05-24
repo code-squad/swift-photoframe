a65e53627a11118f917be3ace30fa463aed57f6d
Moon <myssun0325@gmail.com>
2018-05-24 16:39:16 +0900

    Create PhotoFrame app & change readme file

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by moon on 2018. 5. 24..
+//  Copyright © 2018년 moon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(#file, #line, #function, #column)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by moon on 2018. 5. 24..
+//  Copyright © 2018년 moon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(#file, #line, #function, #column)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
