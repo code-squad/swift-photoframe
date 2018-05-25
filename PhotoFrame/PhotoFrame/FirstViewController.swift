2e39817cb0ea1252a494236f009d176e04da7ded
Moon <myssun0325@gmail.com>
2018-05-25 21:13:17 +0900

    Refactor constant by using nested type(namespace)

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -12,15 +12,6 @@ class FirstViewController: UIViewController {
 
     @IBOutlet weak var photoLabel: UILabel!
     @IBOutlet weak var photoDescription: UILabel!
-
-    private let TITLE: String = "Mason의 사진액자"
-    private let DESCRIPTION: String = "위 작품은 Mason의 사진액자 입니다."
-    private let photoLabelAlpha: CGFloat = 0.7
-    private let photoDescriptionAlpha: CGFloat = 0.5
-    private let photoLabelFont: UIFont! = UIFont(name: "Zapfino", size: 30)
-    private let photoDescriptionFontSize: CGFloat = 20
-    private let photoLabelBackgroundColor: UIColor = UIColor(red: 0, green: 0, blue: 1.0, alpha: 0.3)
-    private let photoDescriptionBackgroundColor: UIColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.3)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,19 +26,46 @@ class FirstViewController: UIViewController {
     }
     
     func setPhotoLabel() {
-        self.photoLabel.text = self.TITLE
-        self.photoLabel.textColor = UIColor.blue
-        self.photoLabel.backgroundColor = self.photoLabelBackgroundColor
-        self.photoLabel.alpha = self.photoLabelAlpha
-        self.photoLabel.font = self.photoLabelFont
+        self.photoLabel.text = LabelText.title
+        self.photoLabel.textColor = LabelTextColor.title
+        self.photoLabel.backgroundColor = LabelBackgroundColor.title
+        self.photoLabel.alpha = LabelAlpha.title
+        self.photoLabel.font = LabelFont.title
     }
     
     func setPhotoDescription() {
-        self.photoDescription.text = self.DESCRIPTION
-        self.photoDescription.textColor = UIColor.red
-        self.photoDescription.backgroundColor = self.photoDescriptionBackgroundColor
-        self.photoDescription.alpha = self.photoDescriptionAlpha
-        self.photoDescription.font = self.photoDescription.font.withSize(self.photoDescriptionFontSize)
+        self.photoDescription.text = LabelText.description
+        self.photoDescription.textColor = LabelTextColor.description
+        self.photoDescription.backgroundColor = LabelBackgroundColor.description
+        self.photoDescription.alpha = LabelAlpha.description
+        self.photoDescription.font = LabelFont.description
     }
 }
 
+extension FirstViewController {
+    
+    enum LabelText {
+        static let title: String = "Mason의 사진액자"
+        static let description: String = "위 작품은 Mason의 사진액자 입니다."
+    }
+    
+    enum LabelTextColor {
+        static let title: UIColor = UIColor.blue
+        static let description: UIColor = UIColor.red
+    }
+    
+    enum LabelAlpha {
+        static let title: CGFloat = 0.7
+        static let description: CGFloat = 0.5
+    }
+    
+    enum LabelFont {
+        static let title: UIFont! = UIFont(name: "Zapfino", size: 30)
+        static let description: UIFont! = UIFont(name: "Zapfino", size: 20)
+    }
+    
+    enum LabelBackgroundColor {
+        static let title: UIColor = UIColor(red: 0, green: 0, blue: 1.0, alpha: 0.3)
+        static let description: UIColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.3)
+    }
+}
