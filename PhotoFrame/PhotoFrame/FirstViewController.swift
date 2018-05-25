2d6a72a33645b1f31709741514be64eb0f520d4c
Moon <myssun0325@gmail.com>
2018-05-25 17:49:51 +0900

    Change constant to let

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -12,12 +12,21 @@ class FirstViewController: UIViewController {
 
     @IBOutlet weak var photoLabel: UILabel!
     @IBOutlet weak var photoDescription: UILabel!
+
+    private let TITLE: String = "Mason의 사진액자"
+    private let DESCRIPTION: String = "위 작품은 Mason의 사진액자 입니다."
+    private let photoLabelAlpha: CGFloat = 0.7
+    private let photoDescriptionAlpha: CGFloat = 0.5
+    private let photoLabelFont: UIFont! = UIFont(name: "Zapfino", size: 30)
+    private let photoDescriptionFontSize: CGFloat = 20
+    private let photoLabelBackgroundColor: UIColor = UIColor(red: 0, green: 0, blue: 1.0, alpha: 0.3)
+    private let photoDescriptionBackgroundColor: UIColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.3)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         print(#file, #line, #function, #column)
         setPhotoLabel()
+        setPhotoDescription()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,11 +35,19 @@ class FirstViewController: UIViewController {
     }
     
     func setPhotoLabel() {
-        self.photoLabel.text = "Mason의 사진액자"
+        self.photoLabel.text = self.TITLE
         self.photoLabel.textColor = UIColor.blue
-        self.photoLabel.backgroundColor = UIColor(red: 0, green: 0, blue: 1.0, alpha: 0.3)
-        self.photoLabel.alpha = 0.7
-        self.photoLabel.font = UIFont(name: "Zapfino", size: 30)
+        self.photoLabel.backgroundColor = self.photoLabelBackgroundColor
+        self.photoLabel.alpha = self.photoLabelAlpha
+        self.photoLabel.font = self.photoLabelFont
+    }
+    
+    func setPhotoDescription() {
+        self.photoDescription.text = self.DESCRIPTION
+        self.photoDescription.textColor = UIColor.red
+        self.photoDescription.backgroundColor = self.photoDescriptionBackgroundColor
+        self.photoDescription.alpha = self.photoDescriptionAlpha
+        self.photoDescription.font = self.photoDescription.font.withSize(self.photoDescriptionFontSize)
     }
 }
 
