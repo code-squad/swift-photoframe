120fbb5af32e3457db1dc8404cf7b079b613bfe1
Moon <myssun0325@gmail.com>
2018-05-25 11:17:01 +0900

    Connect IBOutlet to another label in first scene by using storyboard

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class FirstViewController: UIViewController {
 
     @IBOutlet weak var photoLabel: UILabel!
+    @IBOutlet weak var photoDescription: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
