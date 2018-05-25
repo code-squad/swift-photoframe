3046a5207d8aeefe652a93a7f4425994eb89ad27
Moon <myssun0325@gmail.com>
2018-05-25 17:11:22 +0900

    Add setPhotoLabel method to set first label

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -17,24 +17,20 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         
         print(#file, #line, #function, #column)
-        self.photoLabel.text = "Mason의 사진액자"
-        self.photoLabel.textColor = UIColor.blue
-        self.photoLabel.backgroundColor = UIColor(red: 0, green: 0, blue: 1.0, alpha: 0.3)
-        self.photoLabel.alpha = 0.7
-        self.photoLabel.font = UIFont(name: "Zapfino", size: 30)
-        
-        self.photoDescription.text = "위 작품은 Mason의 사진액자 입니다."
-        self.photoDescription.textColor = UIColor.red
-        self.photoDescription.backgroundColor = UIColor(red: 1.0, green: 0, blue: 0, alpha: 0.3)
-        self.photoDescription.alpha = 0.5
-        self.photoDescription.font = self.photoDescription.font.withSize(20)
+        setPhotoLabel()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func setPhotoLabel() {
+        self.photoLabel.text = "Mason의 사진액자"
+        self.photoLabel.textColor = UIColor.blue
+        self.photoLabel.backgroundColor = UIColor(red: 0, green: 0, blue: 1.0, alpha: 0.3)
+        self.photoLabel.alpha = 0.7
+        self.photoLabel.font = UIFont(name: "Zapfino", size: 30)
+    }
 }
 
