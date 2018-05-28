787df2de152923bdb6e8fb4f99e51db3d892eedb
Moon <myssun0325@gmail.com>
2018-05-28 20:28:26 +0900

    Add IBAction on YelloViewController to dissmiss viewcontroller

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -21,7 +21,9 @@ class YellowViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func closeButtonTouched(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
