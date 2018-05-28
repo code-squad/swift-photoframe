2f0ec70d0877ad74339e263f20a8035706bcc2dc
Moon <myssun0325@gmail.com>
2018-05-28 21:46:25 +0900

    Add print method to check in viewDidLoad

--- PhotoFrame/PhotoFrame/BlueViewController.swift
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -14,6 +14,7 @@ class BlueViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
     }
     
     //MARK: UIViewController life cycle methods
