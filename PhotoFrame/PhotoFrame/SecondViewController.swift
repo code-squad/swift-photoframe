0294a8d97d6dea74fcbad9c9c1a151d3297f576e
DrakeYang <drake.the.programmer@gmail.com>
2018-06-28 20:50:42 +0900

    UILabel 추가 속성 정용

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    @IBOutlet weak var photoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print(#file, #line, #function, #column)
+        self.photoLabel.text = "Drake 의 사진액자"
+//        self.photoLabel.isHighlighted = true
+        self.photoLabel.highlightedTextColor = UIColor.purple
+        self.photoLabel.shadowColor = UIColor.gray
+        self.photoLabel.shadowOffset = CGSize.init(width: 10, height: 20)
     }
 
     override func didReceiveMemoryWarning() {
