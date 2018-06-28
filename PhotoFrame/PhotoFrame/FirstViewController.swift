4766a1fd60841f3a7c1ed1700cf2cb8008fa0184
DrakeYang <drake.the.programmer@gmail.com>
2018-06-28 19:43:41 +0900

    print(#file, #line, #function, #column) 코드 추가

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -12,6 +12,7 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(#file, #line, #function, #column)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
