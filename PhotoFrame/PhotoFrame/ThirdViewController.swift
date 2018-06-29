70be6ea3cbf78489454ca148aeb8378eaeab24a5
DrakeYang <drake.the.programmer@gmail.com>
2018-06-29 13:45:17 +0900

    세번째 뷰를 파일과 연결

---
+++ PhotoFrame/PhotoFrame/ThirdViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ThirdViewController.swift
+//  PhotoFrame
+//
+//  Created by Yoda Codd on 2018. 6. 29..
+//  Copyright © 2018년 Drake. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
