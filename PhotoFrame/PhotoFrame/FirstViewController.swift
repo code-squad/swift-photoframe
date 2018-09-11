65f9b159e16d026660beb08fb81775cead50011c
oingbong <oingbong@gmail.com>
2018-09-11 16:45:07 +0900

    Step2 - IBOutlet

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         print("First : " , #file ,  #line ,  #function , #column)
+        
+        self.firstLabel.text = "오잉봉의 사진액자"
+        self.firstLabel.textColor = UIColor.green
+        self.firstLabel.shadowColor = UIColor.darkGray
+        self.firstLabel.shadowOffset = CGSize.init(width: 5, height: 5)
+        
+        self.firstDescription.textColor = UIColor.red
+        self.firstDescription.lineBreakMode = NSLineBreakMode.byClipping
+        
     }
 
     override func didReceiveMemoryWarning() {
--- README.md
+++ README.md
@@ -8,11 +8,22 @@
 
 `Second :  /Users/oingbong/Documents/CodeSquad/Level3/swift-photoframe/PhotoFrame/PhotoFrame/SecondViewController.swift 16 viewDidLoad() 57`
 
-![Alt text](CaptureImage/Step1_1.png "Step1_1")
-![Alt text](CaptureImage/Step1_2.png "Step1_2")
+![Step1_1](CaptureImage/Step1_1.png)
+![Step1_2](CaptureImage/Step1_2.png)
 
 ### UITabbarController
  - UITabbar 를 포함해 UITabbar 에서 선택한 View 까지 포함하는 전체적인 의미
 
 ### UITabbar
  - 여러 View 중에 하나를 선택할 수 있게 하단 혹은 화면 끝단에 존재하는 라디오 버튼 형식의 바
+
+---
+
+## Step2 - IBOutlet
+
+### UIOutlet
+
+![Step2_1](CaptureImage/Step2_1.png)
+
+---
+
