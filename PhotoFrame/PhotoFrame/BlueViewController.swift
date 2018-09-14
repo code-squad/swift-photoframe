bf4108ba9d7f8d5041e3d7b16e867187e4120899
oingbong <oingbong@gmail.com>
2018-09-14 14:27:35 +0900

    Step5 - Add ViewController

---
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -0,0 +1,33 @@
+//
+//  BlueViewController.swift
+//  PhotoFrame
+//
+//  Created by oingbong on 2018. 9. 14..
+//  Copyright © 2018년 oingbong. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,33 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by oingbong on 2018. 9. 14..
+//  Copyright © 2018년 oingbong. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
