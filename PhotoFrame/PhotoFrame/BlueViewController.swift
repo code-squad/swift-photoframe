399a820ede0ab73b9b6dc6328ebbb582d2deb9e6
oingbong <oingbong@gmail.com>
2018-09-14 14:53:36 +0900

    Edit present View without Storyboard

--- PhotoFrame/PhotoFrame/BlueViewController.swift
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -15,19 +15,20 @@ class BlueViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
     }
-    */
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
 
 }
