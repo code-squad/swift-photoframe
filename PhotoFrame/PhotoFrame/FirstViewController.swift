6f8ca174220ad96a7d0f3243e3f63bb1f772d22c
popsmile <popsmile7@gmail.com>
2018-11-28 13:52:04 +0900

    Create a Tabbed App named PhotoFrame

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by 윤지영 on 28/11/2018.
+//  Copyright © 2018 윤지영. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
