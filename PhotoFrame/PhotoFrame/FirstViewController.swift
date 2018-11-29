cd9dda88b43a5a808ba85fa71a51dd59e62334d0
popsmile <popsmile7@gmail.com>
2018-11-29 16:27:27 +0900

    Add debugging identifiers to print to two ViewControllers

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -12,7 +12,7 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(#file, #line, #function, #column)
     }
 
 
