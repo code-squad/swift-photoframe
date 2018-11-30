95ee5aa2d4cd5861725960c7489c5e41234f6ce5
popsmile <popsmile7@gmail.com>
2018-11-30 13:12:45 +0900

    Create a method and an enum to set styles to the labels

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -11,13 +11,50 @@ import UIKit
 class FirstViewController: UIViewController {
     @IBOutlet weak var photoLabel: UILabel!
     @IBOutlet weak var photoDescription: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.photoLabel.text = "제이미의 사진액자"
         self.photoDescription.text = "아직 추가된 사진이 없습니다."
+        setStyle(of: photoLabel, as: .Title)
+        setStyle(of: photoDescription, as: .SubTitle)
     }
 
+    private func setStyle(of label: UILabel, as customStyle: LabelStyle) {
+        label.textColor = customStyle.textColor
+        customStyle.others(label)
+    }
+
+    private enum LabelStyle {
+        case Title
+        case SubTitle
+    
+        var textColor: UIColor {
+            switch self {
+            case .Title: return UIColor.darkGray
+            case .SubTitle: return UIColor.lightGray
+            }
+        }
+    
+        var others: (UILabel) -> Void {
+            switch self {
+            case .Title:
+                return applyTitleStyle(to:)
+            case .SubTitle:
+                return applySubTitleStyle(to:)
+            }
+        }
+    
+        private func applyTitleStyle(to label: UILabel) {
+            label.textColor = UIColor.darkGray
+            label.shadowColor = UIColor.lightGray
+            label.shadowOffset = CGSize(width: 2, height: 2)
+        }
+    
+        private func applySubTitleStyle(to label: UILabel) {
+            label.font = UIFont.boldSystemFont(ofSize: CGFloat(integerLiteral: 20))
+        }
+    }
 
 }
 
