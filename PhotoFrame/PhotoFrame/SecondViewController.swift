5f59691e37bae2246f15605ff55299638c76ec35
popsmile <popsmile7@gmail.com>
2018-12-07 17:37:21 +0900

    Create an ImageView to Second Scene
    Add a method to show a different random image when the next button is clicked

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        photoLabel.text = "랜덤 사진 앨범"
         print(#file, #line, #function, #column)
     }
 
+    private func makeRandomImage() -> String {
+        let firstImageNumber = 1, lastImageNumber = 22
+        let imageFormat = "jpg"
+        let randomNumber = Int.random(in: firstImageNumber...lastImageNumber)
+        let nameOfImage = "\(randomNumber).\(imageFormat)"
+        return randomNumber < 10 ? "0\(nameOfImage)" : nameOfImage
+    }
+
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+        let randomImage = makeRandomImage()
+        self.photoImageView.image = UIImage(named: randomImage)
+    }
 
 }
 
