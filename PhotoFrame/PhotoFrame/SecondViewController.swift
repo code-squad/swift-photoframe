e220ea76c6bd9fef87529b56c293df159f558fcb
hngfu <djsl1018@naver.com>
2018-12-13 18:13:10 +0900

    README.MD작성

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by 조재흥 on 18. 12. 13..
+//  Copyright © 2018 hngfu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        print(#file, #line, #function, #column)
+    }
+
+
+}
+
