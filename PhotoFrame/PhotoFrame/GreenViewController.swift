5394a53af3659c776758561e0cd362625233f296
hngfu <djsl1018@naver.com>
2018-12-14 15:00:35 +0900

    GreenViewController와 YellowViewController에 화면관련 콜백함수 추가

--- PhotoFrame/PhotoFrame/GreenViewController.swift
+++ PhotoFrame/PhotoFrame/GreenViewController.swift
@@ -16,6 +16,22 @@ class GreenViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
 
     @IBAction func closeButtonTouched(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
