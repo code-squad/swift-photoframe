c56643bc6a470b33f318d9c1c1a741db4f896eb8
hngfu <djsl1018@naver.com>
2018-12-14 14:49:47 +0900

    closeButtonTouched() 구현

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -18,6 +18,7 @@ class YellowViewController: UIViewController {
     
 
     @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     /*
     // MARK: - Navigation
