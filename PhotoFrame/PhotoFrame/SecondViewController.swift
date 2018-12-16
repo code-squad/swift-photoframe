329ea154c3df1d0d575692d2ffc29abee617226f
hngfu <djsl1018@naver.com>
2018-12-17 02:05:27 +0900

    이미지가 비율에 맞게 나오도록 ImageView의 ContentMode를 AspectFit으로 지정

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -14,7 +14,7 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        self.photoImageView.contentMode = .scaleAspectFit
         print(#file, #line, #function, #column)
     }
     
