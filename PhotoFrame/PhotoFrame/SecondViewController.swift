59939a5b177dd3f4db9265720cbe8443c09de6b7
Dongmin.yoon <ehdals8965@naver.com>
2018-12-17 17:14:57 +0900

    print file Location, line number, function name, line column

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by 윤동민 on 17/12/2018.
+//  Copyright © 2018 윤동민. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
