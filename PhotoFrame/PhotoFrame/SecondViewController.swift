5521f4b446e2755321c3ea79b59f888ab6e6c969
Dongmin.yoon <ehdals8965@naver.com>
2018-12-20 17:47:59 +0900

    Add UIImageView  Object

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.photoImageView.image = UIImage(named: "01.jpg")
+        self.photoImageView.contentMode = .scaleAspectFit
     }
 
 
+    @IBAction func nextImageTouched(_ sender: Any) {
+        var photoNumber: [String] = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22"]
+        
+        self.photoImageView.image = UIImage(named: "\(photoNumber[Int(arc4random_uniform(22))]).jpg")
+    }
 }
 
