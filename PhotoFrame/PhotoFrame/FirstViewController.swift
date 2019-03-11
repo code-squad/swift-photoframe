3e00590da2ffbdd136fc224000f636a323567097
conyconydev <conyconydev@gmail.com>
2019-03-11 14:59:36 +0900

    print(#file, #line, #function, #column) 코드를 추가, 실행결과 추가

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -13,8 +13,16 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        https://docs.swift.org/swift-book/ReferenceManual/Expressions.html
+//        #file    String    파일이 나타나는 파일의 이름입니다.
+//        #line    Int    표시되는 행 번호.
+//        #column    Int    열이 시작되는 열 번호입니다.
+//        #function    String    이름이 나타나는 선언의 이름입니다.
+//        #dsohandle    UnsafeRawPointer    DSO (동적 공유 객체)는 사용중인 곳에서 처리합니다.
+        
+        print(#file, #line, #function, #column)
     }
 
 
 }
-
