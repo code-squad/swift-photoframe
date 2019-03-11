fa7f2b8852e87f2dd83de0841cdb1ba5def6c03f
conyconydev <conyconydev@gmail.com>
2019-03-11 14:25:04 +0900

    project Tabbed App 생성

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by Elena on 11/03/2019.
+//  Copyright © 2019 elena. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by Elena on 11/03/2019.
+//  Copyright © 2019 elena. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
