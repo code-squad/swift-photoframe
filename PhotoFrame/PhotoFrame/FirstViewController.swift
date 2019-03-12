cba9e55d3a929026d35094aa8f316c8bbb91e825
conyconydev <conyconydev@gmail.com>
2019-03-12 11:43:37 +0900

    step3 진행 : IBAction 버튼 추가 , readme.md 파일 내용 추가

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -29,5 +29,10 @@ class FirstViewController: UIViewController {
         print(#file, #line, #function, #column)
     }
 
-
+    @IBAction func nextButtonTouched(_ sender: Any) {
+        self.photoLabel.textColor = UIColor.blue
+        self.photoLabel.backgroundColor = UIColor.yellow
+        self.photoLabel.alpha = 0.5
+    }
+    
 }
