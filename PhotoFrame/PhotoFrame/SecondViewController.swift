a157ac24685e4377ee0d4b784f042c582536390b
김지나 <jinagold.k@gmail.com>
2019-03-12 17:31:14 +0900

    PhotoFrame step1.Tabbed App템플릿

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by 김지나 on 11/03/2019.
+//  Copyright © 2019 김지나. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
