e4116307804a211dd398da112dc3a8e38a6dae61
김지나 <jinagold.k@gmail.com>
2019-03-12 17:31:14 +0900

    PhotoFrame step1.Tabbed App템플릿

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by 김지나 on 11/03/2019.
+//  Copyright © 2019 김지나. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(#file, #line, #function, #column)
+        /*
+         #file : /Users/jina/github/swift-photoframe/PhotoFrame/PhotoFrame/FirstViewController.swift
+         #line : 16
+         #function : viewDidLoad()
+         #column : 40 (16번 행의 40번 열에 있다. #column이 시작되는 곳이 40번이다.)
+        */
+    }
+
+
+}
+
