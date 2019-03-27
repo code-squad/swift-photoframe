86f13b4dbd40b00a50100fe34ddccdde8ee3bf58
김지나 <jinagold.k@gmail.com>
2019-03-27 15:51:39 +0900

    콜백함수에 super 호출

--- PhotoFrame/PhotoFrame/MyViewController.swift
+++ PhotoFrame/PhotoFrame/MyViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class MyViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // true로 하면 애니메이션을 이용하여 뷰를 불러온다. 애니메이션이은 창이 보여지는 방법을 의미
+        // false로 하면 기본 형식으로 나타남
         print("이제나타날거야",#file, #line, #function, #column)
     }
     override func viewDidLoad() {
@@ -18,13 +21,16 @@ class MyViewController: UIViewController {
         print("뷰가만들어졌어",#file, #line, #function, #column)
     }
     override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         print("뷰가사라질거야",#file, #line, #function, #column)
     }
     override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
         print("뷰가사라졌어",#file, #line, #function, #column)
     }
     
     @IBAction func closeButtonTouched(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
+        //self.dismiss(animated: true, completion: nil)
+        self.navigationController?.popViewController(animated: true)
     }
 }
