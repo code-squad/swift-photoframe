5d05b3cb1d8128955fd9942daa4e9a808d01a1a6
김지나 <jinagold.k@gmail.com>
2019-03-28 18:17:00 +0900

    Photoframe step6 추가
    segue연결을 제거하고 코드로 구현

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -19,11 +19,12 @@ class FirstViewController: UIViewController {
         self.firstLabel.textColor = UIColor.yellow
         self.firstLabel.backgroundColor = UIColor.blue.withAlphaComponent(0.5)
         self.firstLabel.font = firstLabel.font.withSize(50)
-       
+ 
         self.firstDescription.text = "파덕?????"
         self.firstDescription.textColor = UIColor.blue.withAlphaComponent(0.5)
         self.firstDescription.backgroundColor = UIColor(red:0.97, green:0.98, blue:0.56, alpha:1.0)
         self.firstDescription.font = firstDescription.font.withSize(15)
+ 
     }
     
     @IBAction func nextButtonTouched(_ sender: UIButton) {
@@ -33,15 +34,15 @@ class FirstViewController: UIViewController {
         
         // segue 연결없이 화면 전환! ->>
         // 이동할 뷰컨트롤러 객체를 스토리보드ID를 이용하여 참조
-        //if let vc = self.storyboard?.instantiateViewController(withIdentifier: "greenView") {
-            // 화면을 전환할 때 애니메이션 정의
+        if let vc = self.storyboard?.instantiateViewController(withIdentifier: "greenView") {
+             //화면을 전환할 때 애니메이션 정의
             //vc.modalTransitionStyle = UIModalTransitionStyle.coverVertical
             
-            //인자값으로 받은 뷰 컨트롤러로 화면 이동
+            //인자값으로 받은 컨트롤러로 화면 이동
+            self.navigationController?.pushViewController(vc, animated: true)
             //self.present(vc, animated: true, completion: nil)
-        //}
+            
+        }
     }
-    
 
 }
-
