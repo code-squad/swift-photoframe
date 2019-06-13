7660648df058e15172e9d6c948f0ba65e7ff7da5
kiljh <kiljh@me.com>
2019-06-13 18:07:30 +0900

    수정 - 글자 속성 수정

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -8,10 +8,35 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController:
+    
+    
+    UIViewController {
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    @IBOutlet weak var firstDescr: UILabel!
+    
     override func viewDidLoad() {
-        print(#file, #line, #function, #column)
+        
+        self.firstLabel.text = "도미닉의 사진액자"
+        let fontSize = UIFont.boldSystemFont(ofSize: 60)
+        let descriptionFontSize = UIFont.boldSystemFont(ofSize: 40)
+        let attributedString = NSMutableAttributedString(string: firstLabel.text ?? "도미닉의 사진액자")
+        attributedString.addAttribute(kCTFontAttributeName as NSAttributedString.Key, value: fontSize, range: NSRange(location: 0, length: 3))
+        attributedString.addAttribute(NSAttributedString.Key.backgroundColor, value: UIColor.black.withAlphaComponent(0.5), range: NSRange(location: 5, length: 4))
+        
+        firstLabel.attributedText = attributedString
+        
+        self.firstDescr.text = "바다 사진"
+        let descriptionAttributedString = NSMutableAttributedString(string: firstDescr.text ?? "바다 사진")
+
+        descriptionAttributedString.addAttribute(NSAttributedString.Key.backgroundColor, value: UIColor.blue, range: NSRange(location: 0, length: 2))
+        descriptionAttributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.white, range: NSRange(location: 0, length: 2))
+        descriptionAttributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.red, range: NSRange(location: 3, length: 2))
+        descriptionAttributedString.addAttribute(kCTFontAttributeName as NSAttributedString.Key, value: descriptionFontSize, range: NSRange(location: 0, length: 5))
+        
+        firstDescr.attributedText = descriptionAttributedString
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
