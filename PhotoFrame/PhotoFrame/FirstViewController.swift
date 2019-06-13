c36fee032dee6e60aa0ea678c5ac38d18a0e2c7a
kiljh <kiljh@me.com>
2019-06-13 16:54:56 +0900

    생성 - 프로젝트 생성

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by joon-ho kil on 6/13/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        print(#file, #line, #function, #column)
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
