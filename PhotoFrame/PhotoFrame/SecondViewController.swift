a89ff76d1dc24795c50fc5bfbd166b86d1ea8761
kiljh <kiljh@me.com>
2019-06-13 16:54:56 +0900

    생성 - 프로젝트 생성

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by joon-ho kil on 6/13/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
