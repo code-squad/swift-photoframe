b71d745c9585aeeeabc8253a9f70819d95c0dc43
kiljh <kiljh@me.com>
2019-06-17 15:26:36 +0900

    생성 - YellowViewController, GreenViewController 추가

---
+++ PhotoFrame/PhotoFrame/GreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GreenViewController.swift
+//  PhotoFrame
+//
+//  Created by joon-ho kil on 6/17/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
