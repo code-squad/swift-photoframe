37f084e2a3966110a0be5c8648677b9f518b6b54
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-21 15:25:51 +0900

    segue 없이 뷰컨트롤러를 전환하도록 수정함

--- PhotoFrame/PhotoFrame/BlueViewController.swift
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -16,6 +16,13 @@ class BlueViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        if let viewController = storyboard?.instantiateViewController(withIdentifier: "RedViewController") {
+            present(viewController, animated: true, completion: nil)
+        }
+    }
+    
     @IBAction func closeButtonTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
