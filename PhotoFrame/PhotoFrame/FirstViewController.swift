4e0db9ecb1be69e1f3ae3f8f7de86b15d1b4686f
hwj0623 <hwj0623@naver.com>
2019-06-24 14:14:57 +0900

    STEP 11-2 Label 세팅 수정 및 이론 정리

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -2,7 +2,7 @@
 //  FirstViewController.swift
 //  PhotoFrame
 //
-//  Created by hw on 24/06/2019.
+//  Created by hw on 21/06/2019.
 //  Copyright © 2019 hwj. All rights reserved.
 //
 
@@ -10,12 +10,23 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
     override func viewDidLoad() {
-        print(#file, #line, #function, #column)
+        willSetFirstViewSetting()
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
     }
-
-
+    
+    private func willSetFirstViewSetting() {
+        self.firstLabel.text = "Doran의 사진액자"
+        self.firstLabel.font = UIFont(name: "Times New Roman", size: 19)
+        self.firstLabel.textColor = UIColor(displayP3Red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        self.firstLabel.backgroundColor = UIColor(displayP3Red: 0.6, green: 0.7, blue: 0.65, alpha: 1.0)
+        
+        self.firstDescription.text = "FirstViewController에 의해 로딩"
+        self.firstDescription.font = UIFont(name: "Times New Roman", size: 10)
+        self.firstDescription.textColor = UIColor(displayP3Red: 0.9, green: 0.5, blue: 0.3, alpha: 1.0)
+    }
+    
 }
-
