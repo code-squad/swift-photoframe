dd4e592791af65171d9dac1c798fb7f83ed7ccce
gukjin jang <jgj455@gmail.com>
2019-06-26 00:32:49 +0900

    Modify: UILabel 공부 및 실습

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -13,8 +13,16 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
+        self.photoLabel.text = "Aiden의 사진액자"
+        self.photoLabel.textColor = UIColor.blue
+        self.photoLabel.backgroundColor = UIColor.cyan
+        self.photoLabel.font = UIFont(name: "Apple Color Emoji", size: 30)
+        self.firstDescription.text = "Since 25.06.19"
+        self.firstDescription.font = UIFont.italicSystemFont(ofSize: 20)
+        self.firstDescription.alpha = 0.5
     }
 
-
+    @IBOutlet weak var photoLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
 }
 
