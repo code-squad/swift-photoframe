d1c14fc889a23e187129da9f3db2cfc105ea502c
hwj0623 <hwj0623@naver.com>
2019-06-26 14:59:39 +0900

    STEP 11-5 (3) PurpleViewController 생명주기 콜백함수 추가

---
+++ PhotoFrame/PhotoFrame/PurpleViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PurpleViewController.swift
+//  PhotoFrame
+//
+//  Created by hw on 26/06/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import UIKit
+
+class PurpleViewController: UIViewController {
+
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("PurpleVC viewDidLoad ", #file, #line, #function, #column)
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("PurpleVC viewWillAppear", #file, #line, #function, #column)
+        super.viewWillAppear(true)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("PurpleVC viewDidAppear", #file, #line, #function, #column)
+        super.viewDidAppear(true)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print("PurpleVC viewWillDisappear", #file, #line, #function, #column)
+        super.viewWillDisappear(true)
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print("PurpleVC viewDidDisappear", #file, #line, #function, #column)
+        super.viewDidDisappear(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
