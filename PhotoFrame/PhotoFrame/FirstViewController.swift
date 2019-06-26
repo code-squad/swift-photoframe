809c87be148fc26a131ab005c2fafe1e75d67b58
gukjin jang <jgj455@gmail.com>
2019-06-26 11:18:54 +0900

    Modify: IBOutlet 실습

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    @IBAction func nextButtonTouched(_ sender: Any) {
+        self.photoLabel.textColor = UIColor.blue
+        self.photoLabel.backgroundColor = UIColor.yellow
+        self.photoLabel.alpha = 0.5
+    }
+    @IBOutlet weak var photoLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
         self.photoLabel.text = "Aiden의 사진액자"
-        self.photoLabel.textColor = UIColor.blue
+        self.photoLabel.textColor = UIColor(red: 0.3, green: 0.5, blue: 1.0, alpha: 1)
         self.photoLabel.backgroundColor = UIColor.cyan
         self.photoLabel.font = UIFont(name: "Apple Color Emoji", size: 30)
         self.firstDescription.text = "Since 25.06.19"
@@ -22,7 +29,5 @@ class FirstViewController: UIViewController {
         self.firstDescription.alpha = 0.5
     }
 
-    @IBOutlet weak var photoLabel: UILabel!
-    @IBOutlet weak var firstDescription: UILabel!
 }
 
