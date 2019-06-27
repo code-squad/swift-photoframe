779ea611a60b2638a20a64357fc72fcab9606a98
gukjin jang <jgj455@gmail.com>
2019-06-27 17:47:37 +0900

    Modify: 이미지 추가 및 UIImage에 대한 실습

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+        let fileNumber = Int.random(in: 1...22)
+        let fileName = String(format: "%02d", fileNumber)+".jpg"
+        self.photoImageView.image = UIImage(named: fileName)
+    }
+    @IBOutlet weak var photoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
