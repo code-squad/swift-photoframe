4af729e3e258337ad5272e8b9e2d8bd41adc5ce5
cmindy <minjidev@naver.com>
2019-07-09 19:51:54 +0900

    feat: Add print function to viewDidLoad

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -12,7 +12,8 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print(#file, #line, #function, #column)
     }
 
 
