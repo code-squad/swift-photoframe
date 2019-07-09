a10dc24d3939247da2c4826f098316b0c1cfc4f2
Daheen Lee <allwhite.dev@gmail.com>
2019-07-09 18:48:39 +0900

    Add Tabbed App Template and name project PhotoFrame

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by Daheen Lee on 09/07/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
