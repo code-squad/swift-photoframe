a0674d17bb0d1a732ab837ea027fde60fa8418aa
Daheen Lee <allwhite.dev@gmail.com>
2019-07-09 18:48:39 +0900

    Add Tabbed App Template and name project PhotoFrame

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by Daheen Lee on 09/07/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
