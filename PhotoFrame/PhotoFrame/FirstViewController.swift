10d8e046e1ab79521c3acafd4b1de661cbe5e5f1
cmindy <minjidev@naver.com>
2019-07-10 20:02:48 +0900

    feat: Add label outlets for customize label

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var photoLabel: UILabel!
+    @IBOutlet weak var photoDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(#file, #line, #function, #column)
+        
+        //글자색 배경색 투명도 글자 크기
+        photoLabel.text = "민디의 사진액자🥳"
+        
+        photoDescription.text = "Photo by Mindy"
     }
 
 
