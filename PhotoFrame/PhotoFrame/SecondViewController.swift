2ce2eb2abad2305584bbbcedb355d788de3abf6b
Daheen Lee <allwhite.dev@gmail.com>
2019-07-11 14:20:45 +0900

    Add print function in viewDidLoad() method in SecondeViewController

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -12,7 +12,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
     }
 
 
