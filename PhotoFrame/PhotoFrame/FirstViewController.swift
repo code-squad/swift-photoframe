26e881dd86f6d90e3c1c12e9fae5e1e3eef697b6
Daheen Lee <allwhite.dev@gmail.com>
2019-07-16 11:36:18 +0900

    Set text and attributes to attributedText using NSMutableAttributedString in FirstViewController

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -18,6 +18,19 @@ class FirstViewController: UIViewController {
         firstLabel.textColor = .brown
         firstLabel.textAlignment = .right
         firstLabel.text = "흰의 사진 액자"
+        
+        let description = "NSMutableAttributedString 을 사용하여 설정되었습니다."
+        let attributedString = NSMutableAttributedString(string: description)
+        let yellowBackgroundAttribute: [NSAttributedString.Key : Any] = [
+            NSAttributedString.Key.backgroundColor : UIColor.yellow,
+            NSAttributedString.Key.foregroundColor : UIColor.blue,
+            ]
+        let convertedFirstLabelString = NSString(string: description)
+        
+        attributedString.setAttributes(yellowBackgroundAttribute
+            , range: convertedFirstLabelString.range(of: "NSMutableAttributedString"))
+        
+        firstDescription.attributedText = attributedString
     }
 
 
