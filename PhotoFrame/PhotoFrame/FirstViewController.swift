df36a9ba47f34f3835c5bfb9d1089a2484b7ddd7
Daheen Lee <allwhite.dev@gmail.com>
2019-07-18 23:22:01 +0900

    Add target and action method to first and second button

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -19,6 +19,10 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         initializeFirstLabel()
         initializeFirstDescription()
+        secondButton.addTarget(self, action: #selector(FirstViewController.setTextColorPurple), for: .touchUpInside)
+        secondButton.addTarget(self, action: #selector(FirstViewController.setBackgroundColorYellow(_:)), for: .touchUpInside)
+    }
+    
     private func initializeFirstLabel() {
         firstLabel.text = "흰의 사진 액자"
         firstLabel.textAlignment = .center
@@ -38,6 +42,20 @@ class FirstViewController: UIViewController {
         let convertedFirstLabelString = NSString(string: description)
         attributedString.setAttributes(yellowBackgroundAttribute, range: convertedFirstLabelString.range(of: highlightedWord))
         firstDescription.attributedText = attributedString
+    }
+    
+    @IBAction func setBackgroundColorYellow(_ sender: Any) {
+        self.firstLabel.backgroundColor = UIColor.yellow
+        self.firstLabel.alpha = 0.5
+    }
+    
+    @objc func setTextColorPurple() {
+        self.firstLabel.backgroundColor = nil
+        self.firstLabel.textColor = UIColor.purple
+    }
+    
+    @IBAction func setBackgroundColorGray(_ sender: UIButton) {
+        sender.backgroundColor = UIColor.gray
     }
 }
 
