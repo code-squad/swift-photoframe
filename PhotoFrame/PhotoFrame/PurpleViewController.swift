5c5252ca31557388148c92dd83d19294f6d57583
cmindy <minjidev@naver.com>
2019-07-19 14:33:06 +0900

    feat: Add close button to PurpleVC and dismiss action when it touched

--- PhotoFrame/PhotoFrame/PurpleViewController.swift
+++ PhotoFrame/PhotoFrame/PurpleViewController.swift
@@ -13,4 +13,8 @@ class PurpleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func closedButtonTouched(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 }
