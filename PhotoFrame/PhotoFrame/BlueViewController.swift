3180579205708b634d1524ebdc1fc6b41136f8fe
cmindy <minjidev@naver.com>
2019-07-19 14:42:17 +0900

    feat: Add print literal expressions of view life cycle to BlueVC

--- PhotoFrame/PhotoFrame/BlueViewController.swift
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -12,10 +12,27 @@ class BlueViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
     }
     
     @IBAction func closeButtonTouched(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
 }
