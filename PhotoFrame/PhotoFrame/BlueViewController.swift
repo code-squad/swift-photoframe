91aa4ff7c5f9d2ba41987247f4363a3f9a3d0f41
cmindy <minjidev@naver.com>
2019-07-19 14:20:04 +0900

    feat: Add BlueViewController

---
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BlueViewController.swift
+//  PhotoFrame
+//
+//  Created by CHOMINJI on 19/07/2019.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
