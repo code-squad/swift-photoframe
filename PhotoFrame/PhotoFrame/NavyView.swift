1399da7e9d3a1216f6d6454e60dcca02901dff41
cmindy <minjidev@naver.com>
2019-07-19 16:49:47 +0900

    feat: Add nib and cocoa touch class of NavyView

---
+++ PhotoFrame/PhotoFrame/NavyView.swift
@@ -0,0 +1,21 @@
+//
+//  NavyView.swift
+//  PhotoFrame
+//
+//  Created by CHOMINJI on 19/07/2019.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+class NavyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
