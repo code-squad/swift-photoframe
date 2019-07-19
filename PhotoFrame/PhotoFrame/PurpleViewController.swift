a5c5ef122c8ea51907e954be93ee126647d37e8d
cmindy <minjidev@naver.com>
2019-07-19 15:24:28 +0900

    feat: Add print literal expressions of view life cycle to PurpleVC

--- PhotoFrame/PhotoFrame/PurpleViewController.swift
+++ PhotoFrame/PhotoFrame/PurpleViewController.swift
@@ -12,6 +12,24 @@ class PurpleViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
     }
     
     @IBAction func closedButtonTouched(_ sender: Any) {
