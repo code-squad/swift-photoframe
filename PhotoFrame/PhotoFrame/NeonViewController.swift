e795255cee2dda9731aeaa95e001a6e0d4f6624f
부엉이 <o.o.wl.soft@gmail.com>
2019-07-19 13:10:40 +0900

    feat: Add NeonViewController, Define LifeCycle method

---
+++ PhotoFrame/PhotoFrame/NeonViewController.swift
@@ -0,0 +1,55 @@
+//
+//  NeonViewController.swift
+//  PhotoFrame
+//
+//  Created by 이동영 on 19/07/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import UIKit
+
+class NeonViewController: UIViewController {
+    
+    // MARK: LifeCycle Method
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    // MARK: IBAction
+    @IBAction func exit(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true)
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
