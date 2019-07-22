5a33b6759d9953e7ac93765159e24e236a71a8e1
cmindy <minjidev@naver.com>
2019-07-22 17:03:25 +0900

    feat: Add label to NavyView

--- PhotoFrame/PhotoFrame/NavyView.swift
+++ PhotoFrame/PhotoFrame/NavyView.swift
@@ -10,20 +10,27 @@ import UIKit
 
 class NavyView: UIView {
     private let xibName = "NavyView"
-    
+    private var navyLabel: UILabel!
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         commonInit()
     }
-    
+
     private func commonInit() {
         let view = Bundle.main.loadNibNamed(xibName, owner: self, options: nil)?.first as! UIView
         view.frame = self.bounds
         self.addSubview(view)
+        
+        navyLabel = UILabel(frame: CGRect(x: 100, y: 100, width: 100, height: 60))
+        navyLabel.text = "네이비뷰뷰"
+        navyLabel.textColor = .white
+        self.addSubview(navyLabel)
+        
     }
 }
