fe1dc0bab305f4cad9de81e7552d014c78eb0006
부엉이 <o.o.wl.soft@gmail.com>
2019-07-23 05:26:25 +0900

    docs: Add description of ViewController`s LifeCycle Method

--- PhotoFrame/PhotoFrame/SelectSegueViewController.swift
+++ PhotoFrame/PhotoFrame/SelectSegueViewController.swift
@@ -12,7 +12,28 @@ class SelectSegueViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("\(type(of: self))", #function)
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("\(type(of: self))", #function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("\(type(of: self))", #function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("\(type(of: self))", #function)
+    }
+    
+    override func viewDidDisappear (_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("\(type(of: self))", #function)
     }
 
     @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue) {
