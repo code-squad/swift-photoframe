6f5471800af3c4f7d384204485492a369a8f3273
cmindy <minjidev@naver.com>
2019-07-31 15:56:56 +0900

    feat: Add feature that pick random image and set imageview in SecondVC

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    private let imageExtension = ".jpg"
+    
     @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(#file, #line, #function, #column)
+        
+        photoImageView.contentMode = .scaleAspectFill
     }
     
     @IBAction func nextImageButtonTouched(sender: Any) {
-        
+        let imageName = pickImageName()
+        photoImageView.image = UIImage(named: imageName)
+    }
+    
+    private func pickImageName() -> String {
+        let pickedNumber = Int.random(in: 1...22)
+        let imageName = pickedNumber < 10 ? "0\(pickedNumber)\(imageExtension)" : "\(pickedNumber)\(imageExtension)"
+        return imageName
     }
 }
 
