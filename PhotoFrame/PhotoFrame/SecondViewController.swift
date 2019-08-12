b6af814b55bcbe5510f072acc89de47e38e638ae
Jung Kim <godrm77@gmail.com>
2019-08-12 15:41:25 +0900

    Revert "docs: Add UITabBarController"
    This reverts commit 1a36bc44e82a38760be5ad20343e89963bbd4375.

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -10,15 +10,8 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-    var a : Int { print(#function); return 1 }
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(a)
-        print("file: \(#file)") // 어떤 파일인지 절대경로
-        print("line: \(#line)") // 몇번째 라인인지
-        print("function: \(#function)") // 어떤 함수내에서 호출하는 지
-        print("column: \(#column)")     // 줄에서 몇번째인지
-        
         // Do any additional setup after loading the view.
     }
 
