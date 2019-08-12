18448495d36756ce3e16f3b6702d7f62ef388aef
kimjieun <zieunv@gmail.com>
2019-08-12 17:48:40 +0900

    refactor: Modify viewDidLoad()

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by JieunKim on 12/08/2019.
+//  Copyright © 2019 zieunv. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
