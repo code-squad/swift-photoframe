470be8d8fab6def6ed915b90d1d5a70202cae1c8
kimjieun <zieunv@gmail.com>
2019-08-12 17:48:40 +0900

    refactor: Modify viewDidLoad()

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by JieunKim on 12/08/2019.
+//  Copyright © 2019 zieunv. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
