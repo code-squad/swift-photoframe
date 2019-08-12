0cec7fc559b922f301adca5b89a5b7671c6c9958
Jung Kim <godrm77@gmail.com>
2019-08-12 15:41:25 +0900

    Revert "docs: Add description of ViewController`s Responsibilty"
    This reverts commit 6c1f12c0a2a45ea717f6ee924407f04d236612d6.

--- PhotoFrame/PhotoFrame/CustomSegue.swift
+++ PhotoFrame/PhotoFrame/CustomSegue.swift
@@ -9,22 +9,25 @@
 import UIKit
 
 class CustomSegue: UIStoryboardSegue {
-    
+
     override func perform() {
         let initTransform = self.source.view.subviews[0].transform
         UIView.animate(withDuration: 1.5, delay: 0, options: [.curveEaseInOut], animations: {
             self.source.view.frame.size = .zero
+            
             self.source.view.transform = CGAffineTransform.init(rotationAngle: .pi)
             self.source.view.subviews.forEach{
                 $0.transform =  CGAffineTransform.init(rotationAngle: .pi*(1.5)
                 ) }
         }) { _ in
-            self.source.view.transform = .init(rotationAngle: 0)
+           
+             self.source.view.transform = .init(rotationAngle: 0)
             self.source.view.subviews.forEach{
                 $0.transform =  initTransform
             }
-            self.source.present(self.destination, animated: true)
+        self.source.present(self.destination, animated: true)
         }
+    
         
     }
     
