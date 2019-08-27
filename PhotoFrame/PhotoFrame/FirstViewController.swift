82d5cb982526d553b6987ac329a7184706387fc5
kimjieun <zieunv@gmail.com>
2019-08-27 13:41:56 +0900

    스텝 2 수행

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.firstLabel.text = "Ogi의 사진액자"
+        self.firstDescription.text = "너무 신기하쥬?"
+        firstLabel.textColor = .blue
+        firstDescription.backgroundColor = .yellow
+
         print(#file, #line, #function, #column)
         // Do any additional setup after loading the view.
     }
