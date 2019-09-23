62d10a6dda9411811e62ed37eb75443523b95e4e
Kroisttt <yago0315@gmail.com>
2019-09-23 15:52:36 +0900

    PhotoFrame 프로젝트 생성

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by 이진영 on 23/09/2019.
+//  Copyright © 2019 Kroist. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
