df1a6169dd722c8f8ac3ab89b211c4599a01c876
Kroisttt <yago0315@gmail.com>
2019-10-21 16:15:10 +0900

    viewDidLoad() 수정
    UILabel 속성 코드 추가

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        print(#file, #line, #function, #column)
+        self.firstLabel.text = "bean의 사진 액자"
+        self.firstLabel.textColor = .blue
+        
+        self.firstDescription.text = "사진 액자 입니다."
+        self.firstDescription.font = UIFont.systemFont(ofSize: 50)
     }
 
 
