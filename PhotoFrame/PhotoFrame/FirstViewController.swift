2698db7e621bbf9e3cd239b309966a49f4e964dd
corykim0829 <corykim0829@gmail.com>
2020-02-03 23:01:01 +0900

    Add: UIButton IBAction

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -26,4 +26,12 @@ class FirstViewController: UIViewController {
         photoDescription.font = UIFont.boldSystemFont(ofSize: 14)
         photoDescription.alpha = 0.7
     }
+    
+    @IBAction func nextButtonTouched(_ sender: Any) {
+        photoLabel.textColor = .darkGray
+        photoLabel.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+        photoLabel.alpha = 0.9
+        photoDescription.textColor = #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)
+        photoDescription.font = .systemFont(ofSize: 17)
+    }
 }
