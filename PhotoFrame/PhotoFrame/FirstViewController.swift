c40baa2c9f3d394a3f4626be85c0536f0840095e
delma <sdy2856@gmail.com>
2020-02-03 18:23:17 +0900

    Connect IBOutlet and change properties

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet var firstLabel: UILabel!
+    @IBOutlet var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
+        setUpUI()
     }
 
+    func setUpUI(){
+        self.firstLabel.text = "delma의 사진액자"
+        self.firstLabel.textColor = .blue
+        self.firstLabel.font = .boldSystemFont(ofSize: 50.0)
+        
+        self.firstDescription.text = "사진 액자 설명"
+        self.firstDescription.textColor = .green
+    }
 
 }
 
--- README.md
+++ README.md
@@ -10,3 +10,17 @@
 
 
 ![스크린샷 2020-02-03 오후 5.56.42](/Users/adorabledy/Desktop/delma/iOSClass/swift-photoframe/screenshots/스크린샷 2020-02-03 오후 5.56.42.png)
+
+
+
+
+
+# IBOutlet
+
+- `First Scene`  의 레이블을 IBOutlet으로 연결
+- 연결한 아웃렛 변수에 값을 변경
+- 레이블의 속성 변경
+
+# ![스크린샷 2020-02-03 오후 6.18.09](/Users/adorabledy/Desktop/delma/iOSClass/swift-photoframe/screenshots/스크린샷 2020-02-03 오후 6.18.09.png)
+
+![스크린샷 2020-02-03 오후 6.18.21](/Users/adorabledy/Desktop/delma/iOSClass/swift-photoframe/screenshots/스크린샷 2020-02-03 오후 6.18.21.png)
