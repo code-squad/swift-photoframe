12b2390a8575e382ddc3cce7647cbe7a3ada2aa7
GangWoon <gangwoon2018@gmail.com>
2020-02-03 11:51:54 +0900

    PhotoFrame - Viewcontroller programing

---
+++ PhotoFrame/PhotoFrame/BrownViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BrownViewController.swift
+//  PhotoFrame
+//
+//  Created by Cloud on 2020/02/03.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+
+class BrownViewController: UIViewController {
+    
+    @IBAction func ClosedButtonTouched(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
