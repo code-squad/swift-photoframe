7011f03ce059197e7697edc953f2bcb303d8c4b6
seizze <cwcwc3@gmail.com>
2020-02-03 11:01:52 +0900

    PhotoFrame 프로젝트 생성

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by Chaewan Park on 2020/02/03.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
