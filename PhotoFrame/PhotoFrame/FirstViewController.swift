e18fa47f388d7182deb7765f3f13ae9f32cdb328
dayeong <sdy2856@gmail.com>
2020-02-03 11:20:32 +0900

    make project

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by delma on 03/02/2020.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by delma on 03/02/2020.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
