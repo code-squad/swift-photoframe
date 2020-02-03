e27f5c912002f6c69eceb4d70a3f7b0333a4b58c
Anna <bluewhale0528@naver.com>
2020-02-03 20:44:12 +0900

    프로젝트 생성 및 print 코드 추가

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by JW on 2020/02/03.
+//  Copyright © 2020 JW. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file,#line,#function,#column)
+    }
+}
---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by JW on 2020/02/03.
+//  Copyright © 2020 JW. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
