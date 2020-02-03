17046b89cf3a85090685baee2ea2d9653a777ac0
GangWoon <gangwoon2018@gmail.com>
2020-02-03 11:42:47 +0900

    PhotoFrame - IBOutlet

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    @IBOutlet var photoLabel: UILabel!
+    @IBOutlet var photoDescription: UILabel!
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
+        photoLabel.text = "GangWoon의 사진액자"
+        photoDescription.text = "굿드"
     }
-
+    
+    // MARK: - Actions
 
 }
 
