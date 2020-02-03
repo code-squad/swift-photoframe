6ce69114a64554411b5d17e2f544c49bb3954046
hanseop <hanseop95@gmail.com>
2020-02-04 00:11:55 +0900

    버튼 기능 추가
    IBAction을 통해 이벤트가 일어났을 경우 호출되는 Action을 정의하고, 이벤트 발생 시 IBOutlet 변수에 접근하여 값 변경

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -10,17 +10,30 @@ import UIKit
 
 class FirstViewController: UIViewController {
     @IBOutlet weak var firstLabel: UILabel!
+    @IBAction func nextButtonTouched(_ sender: Any) {
+        self.firstLabel.textColor=UIColor.blue
+        self.firstLabel.backgroundColor=UIColor.yellow
+    }
+    
+    @IBAction func secondEventBtn(_ sender: Any) {
+        self.firstLabel.alpha=0.5
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
+        changeFont(self.firstLabel)
         
-        self.firstLabel.text="Olaf의 사진액자"
-        self.firstLabel.alpha=0.5
-        self.firstLabel.backgroundColor=UIColor(displayP3Red: 0.5, green: 0.3, blue: 0.7, alpha: 0.3)
-        self.firstLabel.textColor=UIColor(displayP3Red: 0.0, green: 0.3, blue: 0.5, alpha: 1)
-        self.firstLabel.font=UIFont.systemFont(ofSize: CGFloat(50))
     }
-
-
+    
+    func changeFont(_ label:UILabel){
+        label.text="Olaf의 사진액자"
+        label.alpha=0.3
+        label.backgroundColor=UIColor(displayP3Red: 0.5, green: 0.3, blue: 0.7, alpha: 0.3)
+        label.textColor=UIColor(displayP3Red: 0.0, green: 0.3, blue: 0.5, alpha: 1)
+        label.font=UIFont.systemFont(ofSize: CGFloat(50))
+    }
+    
+    
 }
 
