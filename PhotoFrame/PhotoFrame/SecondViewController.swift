293c39db299ed905d6054a20b8231dbf817ddefd
corykim0829 <corykim0829@gmail.com>
2020-02-03 11:00:36 +0900

    Initialize project and print info

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by Cory Kim on 2020/02/03.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
