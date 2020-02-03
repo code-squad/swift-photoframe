3724580823a094f90534aabdd3b43c861daddd8d
ehgud0670 <kimdo2297@gmail.com>
2020-02-03 11:42:08 +0900

    Add : 콘솔에 자신의 파일 위치, 현재 코드의 행, 현재 함수의 이름, 현재 코드의 시작한 열이 출력되도록 기능 추가

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by kimdo2297 on 2020/02/03.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+}
+
