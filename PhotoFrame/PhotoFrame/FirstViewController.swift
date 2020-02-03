59771b94a66ed68d7f2a6f8c8c79a28821db5ad4
corykim0829 <corykim0829@gmail.com>
2020-02-03 11:00:36 +0900

    Initialize project and print info

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by Cory Kim on 2020/02/03.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(#file, #line, #function, #column)
+    }
+}
