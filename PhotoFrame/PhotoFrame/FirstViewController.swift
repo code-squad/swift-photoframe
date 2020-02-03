f2bb36d793b2fd60663950ddfb33099aa1da8528
dev-Lena <keunnalee@gmail.com>
2020-02-04 01:37:13 +0900

    Lable을 IBOutlet으로 연결 후 아울렛 변수 값 변경
    First Scene에 만들어져 있는 레이블들(firstLabel, firstDescription)을 IBOutlet으로 연결 후 연결한 아웃렛 변수에 값을 변경.

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -10,12 +10,23 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    @IBOutlet weak var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file,#line,#function,#column)
         // Do any additional setup after loading the view.
+        self.firstLabel.text = "First iOS"
+        self.firstLabel.textColor = .systemIndigo
+        self.firstLabel.backgroundColor = .white
+        self.firstLabel.alpha = 0.3
+        self.firstLabel.adjustsFontSizeToFitWidth = true
+        self.firstLabel.numberOfLines = 2
+        self.firstLabel.font = .boldSystemFont(ofSize: 17.0)
+        self.firstDescription.text = "hello world"
+        self.firstDescription.backgroundColor = .systemIndigo
+        self.firstDescription.textColor = .white
     }
-
-
 }
-
--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 2.  (21:30) ViewController 클래스 viewDidLoad() 함수에서 `print(#file, #line, #function, #column)` 코드를 추가 후 실행 콘솔 영역 출력 확인
    ![image-20200203212810017](/Users/keunnalee/Library/Application Support/typora-user-images/image-20200203212810017.png)
 
+3. (22:00) First Scene에 만들어져 있는 레이블들(firstLabel, firstDescription)을 IBOutlet으로 연결 후 연결한 아웃렛 변수에 값을 변경.
+
+   ![image-20200203220128744](/Users/keunnalee/Library/Application Support/typora-user-images/image-20200203220128744.png)
