a7da8b8283ff5a69c29ad90d59c4cb9395219650
seizze <cwcwc3@gmail.com>
2020-02-03 12:42:05 +0900

    FirstViewController의 레이블들 셋업

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var photoLabel: UILabel!
+    @IBOutlet weak var photoDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print(#file, #line, #function, #column)
+        setupLabels()
     }
-
-
+    
+    private func setupLabels() {
+        self.photoLabel.text = "하이디의 사진액자"
+        self.photoLabel.font = .boldSystemFont(ofSize: 26)
+        self.photoLabel.textColor = .systemGreen
+        self.photoLabel.shadowColor = .lightGray
+        self.photoLabel.shadowOffset = CGSize(width: 1, height: 1)
+        
+        self.photoDescription.text = "하이디의 사진액자의 설명"
+        self.photoDescription.textColor = .darkGray
+    }
 }
 
