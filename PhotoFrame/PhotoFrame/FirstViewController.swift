38e973f5e1d663ea17fc126f15ed2d48276a049e
kjoonk <kjoonk@gmail.com>
2020-02-03 11:37:37 +0900

    Tap App 템플릿 생성
    - ViewController.viewDidLoad()에 print() 추가

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by Viet on 2020/02/03.
+//  Copyright © 2020 Viet. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
+    }
+}
+
---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by Viet on 2020/02/03.
+//  Copyright © 2020 Viet. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
