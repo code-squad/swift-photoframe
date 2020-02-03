6979a3b9b7fe014c7e1e24cbab23dbbb059e69a5
seizze <cwcwc3@gmail.com>
2020-02-03 11:50:48 +0900

    viewDidLoad에 콘솔 프린트 함수 추가

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -13,6 +13,7 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
     }
 
 
