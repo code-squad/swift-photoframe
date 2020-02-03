931b722eb6f3bda30ae12b46175751538151a8fd
delma <sdy2856@gmail.com>
2020-02-03 19:04:53 +0900

    connect IBOutlet

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet var firstLabel: UILabel!
+    @IBOutlet var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
+        setUI()
     }
 
-
+    func setUI(){
+        self.firstLabel.text = "delma의 사진액자"
+        self.firstLabel.textColor = .blue
+        self.firstLabel.font = .boldSystemFont(ofSize: 50.0)
+        
+        self.firstDescription.text = "사진 설명"
+        self.firstDescription.textColor = .green
+    }
 }
 
