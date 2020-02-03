2de0829b1888dbd66c089a3df7a6e7d4b73e0448
TTOzzi <devttozzi@gmail.com>
2020-02-03 11:07:04 +0900

    PhotoFrame 프로젝트 생성

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by TTOzzi on 2020/02/03.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
