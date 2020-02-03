3d8253d946e9126743ad22ee04fe8ac9dae6e6be
dev-Lena <keunnalee@gmail.com>
2020-02-03 11:57:06 +0900

    브랜치 생성 및 프로젝트 생성
    Git Hub 아이디(dev-Lena)를 이름으로 하는 브랜치를 생성하고 PhotoFrame이라는 이름의 xcode 프로젝트 생성

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by Keunna Lee on 2020/02/03.
+//  Copyright © 2020 Keunna Lee. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file,#line,#function,#column)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by Keunna Lee on 2020/02/03.
+//  Copyright © 2020 Keunna Lee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
