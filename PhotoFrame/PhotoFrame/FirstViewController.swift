66ca9aa88ae5d0e7c6d1f8d65290547e160ca5b3
1Consumption <hanseop95@gmail.com>
2020-02-03 19:16:42 +0900

    기능 추가
    firstScene의 Label값 변경

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    @IBOutlet weak var firstLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
+        
+        self.firstLabel.text="Olaf의 사진액자"
+        self.firstLabel.alpha=0.5
+        self.firstLabel.backgroundColor=UIColor(displayP3Red: 0.5, green: 0.3, blue: 0.7, alpha: 0.3)
+        self.firstLabel.textColor=UIColor(displayP3Red: 0.0, green: 0.3, blue: 0.5, alpha: 1)
+        self.firstLabel.font=UIFont.systemFont(ofSize: CGFloat(50))
     }
 
 
