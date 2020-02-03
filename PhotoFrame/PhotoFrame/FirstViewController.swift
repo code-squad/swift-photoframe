e287c02477ded75610a0549d3223488e2cdd246b
ehgud0670 <kimdo2297@gmail.com>
2020-02-03 20:24:50 +0900

    Add : 라벨의 속성 변경하는 기능들 추가       1. 라벨의 텍스트를 설정하는 기능 추가       2. 라벨의 색상을 설정하는 기능 추가       3. 라벨의 부분 텍스트의 크기와 폰트 스타일을 설정하는 기능 추가

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
+        
+        customizeFirstLabel()
+    }
+    
+    private func customizeFirstLabel() {
+        setText(label : firstLabel, text : "Jason의 사진 액자")
+        setColor(label : firstLabel, color : UIColor.green)
+        setLableFontAndSizePartly(label: firstLabel,
+                                  name: "Apple Color Emoji",
+                                  size: 50,
+                                  range: (firstLabel.text! as NSString).range(of: "Jason"))
+    }
+    
+    private func setText(label : UILabel, text : String) {
+        label.text = text
+    }
+    
+    private func setColor(label : UILabel, color : UIColor) {
+        label.textColor = color
+    }
+    
+    private func setLableFontAndSizePartly(label : UILabel, name fontName : String, size : CGFloat, range : NSRange) {
+        if let font = UIFont(name: fontName, size: size) {
+            let attributedStr = NSMutableAttributedString(string: label.text!)
+            attributedStr.addAttribute(kCTFontAttributeName as NSAttributedString.Key,
+                                       value: font,
+                                       range: range)
+            label.attributedText = attributedStr
+        }
     }
 }
 
