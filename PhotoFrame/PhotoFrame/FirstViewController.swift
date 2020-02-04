584b15c2b1b1202f1f3db23a5675d78f8f36624e
idevjinie <idevjinie@gmail.com>
2020-02-04 16:32:17 +0900

    First Scene의 레이블을 IBOutlet으로 연결

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(#file, #line, #function, #column)
     }
 
-
 }
-
