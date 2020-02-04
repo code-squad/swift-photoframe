bfbf909fc52be7e77eb6fd6cd00fc638a52e8951
TTOzzi <devttozzi@gmail.com>
2020-02-04 13:56:41 +0900

    화면 관련 콜백함수 추가
    print(#file, #line, #function, #column) 코드와 breakpoint 로 잘 동작하는지 확인

--- PhotoFrame/PhotoFrame/PurpleViewController.swift
+++ PhotoFrame/PhotoFrame/PurpleViewController.swift
@@ -16,6 +16,26 @@ class PurpleViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
     @IBAction func closeButtonTouched(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
