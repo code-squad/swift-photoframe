c044ad234f3d730d170512b9d8a09aed126a0e27
dev-Lena <keunnalee@gmail.com>
2020-02-04 15:13:55 +0900

    버튼 추가및  IBAction 연결

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -13,12 +13,18 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var firstLabel: UILabel!
     
     @IBOutlet weak var firstDescription: UILabel!
+  
+    @IBAction func nextButtonTouched(_ sender: Any) {
+        self.firstLabel.textColor = UIColor.blue
+        self.firstLabel.backgroundColor = UIColor.yellow
+        self.firstLabel.alpha = 0.5
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file,#line,#function,#column)
-        // Do any additional setup after loading the view.
-        self.firstLabel.text = "First iOS"
+
+        self.firstLabel.text = "Lena's PhotoApp"
         self.firstLabel.textColor = .systemIndigo
         self.firstLabel.backgroundColor = .white
         self.firstLabel.alpha = 0.3
