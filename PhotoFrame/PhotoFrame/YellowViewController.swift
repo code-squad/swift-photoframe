d5b5bec8dc1d92764672f9ef2a311ec87ccba99b
1Consumption <hanseop95@gmail.com>
2020-02-04 19:37:38 +0900

    scene과 스크립트 연결
    viewController 클래스와 segue를 연결, button에 이벤트 지정

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,21 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by 신한섭 on 2020/02/04.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    @IBAction func closeButtonTouched(_ sender: Any) {
+           self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
