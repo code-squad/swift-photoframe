b6be740e54d997aa97c3dfc465fadc0ad95c04cc
dev-Lena <keunnalee@gmail.com>
2020-02-04 18:12:15 +0900

    ViewController 클래스와 스토리보드 Scene  연결 후 닫기 버튼을 만들어 화면을 닫는 액션 추가

---
+++ PhotoFrame/PhotoFrame/PurpleViewController.swift
@@ -0,0 +1,42 @@
+//
+//  FourthViewController.swift
+//  PhotoFrame
+//
+//  Created by Keunna Lee on 2020/02/04.
+//  Copyright © 2020 Keunna Lee. All rights reserved.
+//
+
+import UIKit
+
+class PurpleViewController: UIViewController {
+
+    @IBOutlet weak var closeButton: UIButton!
+    
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+    print("PurpleViewController - viewWillAppear: ")
+           print(#file, #line, #function, #column)
+       }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    print("PurpleViewController - viewDidLoad: ")
+               print(#file, #line, #function, #column)
+        self.closeButton.setTitleColor(.white, for: .normal)
+        self.closeButton.setTitle("[닫기]", for: .normal)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        print("PurpleViewController - viewWillDisappear: ")
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("PurpleViewController - viewDidDisappear: ")
+        print(#file, #line, #function, #column)
+    }
+}
