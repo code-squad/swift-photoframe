4cdbf905ac31b9c06b1ddba4aa75ecbe82854a30
TTOzzi <devttozzi@gmail.com>
2020-02-04 13:42:22 +0900

    추가 화면에 닫기 버튼 추가
    추가 화면들에 ViewController 를 지정해줌
    닫기 버튼 추가 후 액션 연결

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,33 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by TTOzzi on 2020/02/04.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
