2f2eada261e84e773f4066d8f724c9148764fae7
seizze <cwcwc3@gmail.com>
2020-02-04 14:36:10 +0900

    feat: Add a button for dismissing green VC
    Create custom view controller class and implement action method that closes green VC

---
+++ PhotoFrame/PhotoFrame/GreenViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GreenViewController.swift
+//  PhotoFrame
+//
+//  Created by Chaewan Park on 2020/02/04.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
