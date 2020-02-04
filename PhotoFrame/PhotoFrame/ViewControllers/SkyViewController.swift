7a01731807f7f7211f4f9268209195f91ee50ef8
corykim0829 <corykim0829@gmail.com>
2020-02-04 14:36:37 +0900

    Add view life-cycle callback functions to Orange, Sky VC

--- PhotoFrame/PhotoFrame/ViewControllers/SkyViewController.swift
+++ PhotoFrame/PhotoFrame/ViewControllers/SkyViewController.swift
@@ -12,7 +12,23 @@ class SkyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("Sky-viewDidLoad\n", #file, #line, #function, #column)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("Sky-viewWillAppear\n", #file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("Sky-viewDidAppear\n", #file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("Sky-viewWillDisappear\n", #file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("Sky-viewDidDisappear\n", #file, #line, #function, #column)
     }
     
     @IBAction func closeButtonTouched(_ sender: Any) {
