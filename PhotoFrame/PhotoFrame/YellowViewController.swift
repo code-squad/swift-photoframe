54fa4b9059c5873e8c645a3e8bb56b3c061f64df
limwin94 <amuguna94@gmail.com>
2020-02-04 19:02:40 +0900

    닫기 버튼 추가 및 메소드 구현

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -15,8 +15,11 @@ class YellowViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+  
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     
-
     /*
     // MARK: - Navigation
 
