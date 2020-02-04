c0d110e26eb6f1688a8dda122d23a97e81b2652e
corykim0829 <corykim0829@gmail.com>
2020-02-04 18:38:00 +0900

    Set UIImagePickerViewDelegate
    - Inherit UIPickerViewDelegate, UINavigationControllerDelegate
    - Set pickerView's delegate as self
    - Use function: imagePickerController()

--- PhotoFrame/PhotoFrame/ViewControllers/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/ViewControllers/SecondViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let imagePickerController = UIImagePickerController()
     @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        imagePickerController.delegate = self
         setupUI()
     }
     
@@ -39,7 +41,13 @@ class SecondViewController: UIViewController {
     }
     
     @IBAction func selectButtonTouched(_ sender: Any) {
-        
+        imagePickerController.sourceType = .photoLibrary
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.allowsEditing = true
+        guard let image = info[.editedImage] as? UIImage else { return }
+        photoImageView.image = image
     }
 }
-
