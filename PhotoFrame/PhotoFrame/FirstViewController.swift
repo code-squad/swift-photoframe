4efc13605c8961e7ae7765f8b564aaf1d93c527a
idevjinie <idevjinie@gmail.com>
2020-02-04 12:06:03 +0900

    viewDidLoad() 함수에 코드 추가 및 README.md 파일 수정

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by jinie on 2020/02/04.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+
+
+}
+
---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by jinie on 2020/02/04.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
