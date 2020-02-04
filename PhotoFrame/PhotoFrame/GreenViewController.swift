7fe594adf7cfdd530b3fc67a035315c3a393fbf0
moss <kjoonk@gmail.com>
2020-02-04 14:40:26 +0900

    GreenViewController 추가
    - 녹색과 노란색 뷰를 컨트롤
    - 닫기 버튼에 액션 추가
    - 뷰 생명 주기에 관한 함수 로그 추가

---
+++ PhotoFrame/PhotoFrame/GreenViewController.swift
@@ -0,0 +1,53 @@
+//
+//  GreenViewController.swift
+//  PhotoFrame
+//
+//  Created by Viet on 2020/02/04.
+//  Copyright © 2020 Viet. All rights reserved.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#file, #line, #function, #column)
+
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
