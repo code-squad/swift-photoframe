0cc767fe32c135b033e8b86050fed4a12dbb67e1
corykim0829 <corykim0829@gmail.com>
2020-02-04 14:28:22 +0900

    Add a close button & close action to dismiss again

---
+++ PhotoFrame/PhotoFrame/ViewControllers/SkyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SkyViewController.swift
+//  PhotoFrame
+//
+//  Created by Cory Kim on 2020/02/04.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class SkyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
