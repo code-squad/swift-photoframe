602f86e9e21f134c11853399ce7b3c5403a28021
limwin94 <amuguna94@gmail.com>
2020-02-04 14:14:04 +0900

    Label을 ViewController에 IBOutlet으로 연결 및 값 수정

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -10,14 +10,32 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var photoLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        /*
         print(#file)
         print(#line)
         print(#function)
         print(#column)
+        */
+        
+        self.photoLabel.text = "Limwin의 사진액자"
+        self.photoLabel.textColor = UIColor.white
+        //아니면 UIFont에서 name에 bold체를 가진 폰트를 써주면서 설정하기도 가능.
+        self.photoLabel.font = UIFont.boldSystemFont(ofSize: 40.0)
+        //기본 background가 white으로 되어있었음
+        //그래서 얘만 .clear해줘야지 보였던것.
+        //self.photoLabel.backgroundColor = .clear
+        
+        self.firstDescription.text = "FirstView에서 실행중입니다."
+        self.firstDescription.textColor = UIColor.white
+        self.firstDescription.font = UIFont(name: "Helvetica Neue", size: 20)
+        
+        self.view.backgroundColor = UIColor.black
     }
 
 
--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 1. Tabbed App 프로젝트 생성
 2. viewDidLoad 함수에 코드 추가 및 실행
 <img width="1147" alt="스크린샷 2020-02-03 오후 12 33 32" src="https://user-images.githubusercontent.com/49548908/73704162-7b8e8d00-4735-11ea-94d7-dbaa0e86512d.png">
+   
+2020.2.4
+1. FirstScene에 만들어져 있는 두개의 레이블을 @IBOutlet으로 연결
+2. text내용, 화면 배경색, 폰트색상, 폰트굵기 변경
+<img width="454" alt="스크린샷 2020-02-04 오후 2 01 54" src="https://user-images.githubusercontent.com/49548908/73715339-0338c380-4757-11ea-8b8a-62ad89cef14f.png">
