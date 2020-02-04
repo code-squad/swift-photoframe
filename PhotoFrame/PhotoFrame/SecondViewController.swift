820664a7b0cfc4a177acb7de67401af3d81d060e
delma <sdy2856@gmail.com>
2020-02-04 16:59:50 +0900

    second view controller에 액자 동작 구현

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    var images:[String] = []
+    @IBOutlet var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        makeImageName()
+        self.photoImageView.image = UIImage(named: "01.jpg")
+        self.photoImageView.contentMode = .scaleToFill
 
+    }
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+        self.photoImageView.image = UIImage(named: images.randomElement()!)
+    }
+    
+    func makeImageName(){
+        for index in 1...22 {
+            if index < 10 {
+                images.append("0\(index).jpg")
+            }else {
+                images.append("\(index).jpg")
+            }
+        }
+    }
 
 }
 
