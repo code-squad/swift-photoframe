55245fe753e66bf860477f935a468833638e2931
corykim0829 <corykim0829@gmail.com>
2020-02-04 14:20:43 +0900

    Add a new VC file & group VCs

---
+++ PhotoFrame/PhotoFrame/ViewControllers/OrangeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OrangeViewController.swift
+//  PhotoFrame
+//
+//  Created by Cory Kim on 2020/02/04.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
