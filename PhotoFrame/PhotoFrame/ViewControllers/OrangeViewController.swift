4cddedf42c8417e7ae2f32efea3596a19e4c7b3b
corykim0829 <corykim0829@gmail.com>
2020-02-04 14:25:36 +0900

    Add a close button & close action to dismiss

--- PhotoFrame/PhotoFrame/ViewControllers/OrangeViewController.swift
+++ PhotoFrame/PhotoFrame/ViewControllers/OrangeViewController.swift
@@ -13,6 +13,9 @@ class OrangeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+    }
+    
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
