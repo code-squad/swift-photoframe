8da3beb7bdc12dec8b656e01b5441761493c95d8
dev-Lena <keunnalee@gmail.com>
2020-02-05 02:05:35 +0900

    generateRandomImage() 메소드 생성

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -15,15 +15,24 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var photoAlbumText: UILabel!
     
     @IBAction func nextImageButtonTouched(_ sender: UIButton) {
+        self.photoImageView.image = generateRandomUIImage()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.photoAlbumText.text = "Photo Album"
+         self.photoImageView.image = generateRandomUIImage()
     }
     
     func generateRandomNumber()->Int{
     return Int(arc4random_uniform(21) + 01)
+    }
+    
+    func generateRandomUIImage() -> UIImage {
+        let images = (01 ... 22).compactMap{
+            $0 < 10 ? UIImage(named: "0\($0).jpg") : UIImage(named: "\($0).jpg")
+           }
+        return images[generateRandomNumber()]
     }
 }
 
