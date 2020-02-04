44353e2ed93ffe390444f2957d068b4e9d94e515
idevjinie <idevjinie@gmail.com>
2020-02-04 16:36:46 +0900

    연결한 아웃렛 변수에 값 할당

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -15,6 +15,11 @@ class FirstViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        firstLabel.text = "jinie의 사진액자"
+        firstDescription.text = "2020 jinie"
+        firstDescription.textColor = .white
+        firstDescription.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+        firstDescription.font = .systemFont(ofSize: 18)
     }
 
 }
