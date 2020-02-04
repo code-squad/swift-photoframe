e2afe6daf9e2c7a578dddf3dd9945dd3038695f6
seizze <cwcwc3@gmail.com>
2020-02-04 14:46:56 +0900

    feat: Add a button for dismissing yellow VC
    Create custom view controller class and implement action method that closes yellow VC

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,22 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by Chaewan Park on 2020/02/04.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
