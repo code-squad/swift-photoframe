f019199226b8f8c9950945a8c727e0cdbfde87c0
Anna <bluewhale0528@naver.com>
2020-02-04 21:23:03 +0900

    ApricotViewController 생성

---
+++ PhotoFrame/PhotoFrame/ApricotViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ApricotViewController.swift
+//  PhotoFrame
+//
+//  Created by JW on 2020/02/04.
+//  Copyright © 2020 JW. All rights reserved.
+//
+
+import UIKit
+
+class ApricotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
