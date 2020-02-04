c10dbc22130a01e1c1097b879632b2755df15ffb
limwin94 <amuguna94@gmail.com>
2020-02-04 15:12:54 +0900

    Label과 FirstViewController 연결 및 수정

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    @IBOutlet weak var photoLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        /*
         print(#file)
         print(#line)
         print(#function)
         print(#column)
+        */
+        self.photoLabel.text = "Limwin의 사진액자"
+        self.photoLabel.textColor = .white
+        self.photoLabel.backgroundColor = .clear
+        self.photoLabel.font = UIFont.boldSystemFont(ofSize: 40)
+        
+        self.firstDescription.text = "FirstViewController에서 실행중입니다."
+        self.firstDescription.textColor = .white
+        view.backgroundColor = .black
+        
     }
-
-
 }
 
--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 1. Tabbed App 프로젝트 생성
 2. viewDidLoad 함수에 코드 추가 및 실행
 <img width="1147" alt="스크린샷 2020-02-03 오후 12 33 32" src="https://user-images.githubusercontent.com/49548908/73704162-7b8e8d00-4735-11ea-94d7-dbaa0e86512d.png">
+
+2020.2.4
+1. FirstView의 2개의 Label을 FirstViewController로 @IBOutlet을 사용해 연결
+2. 연결한 Label에 대해서 배경색상, 폰트색상, 폰트크기, 폰트굵기, 내용 수정
+<img width="454" alt="스크린샷 2020-02-04 오후 3 09 04" src="https://user-images.githubusercontent.com/49548908/73718677-932f3b00-4760-11ea-8d56-5a823b3d61f6.png">
+
