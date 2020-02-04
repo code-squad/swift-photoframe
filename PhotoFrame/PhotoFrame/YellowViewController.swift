d17eb89e56677a17ddae3a30bcd891fccc4f6926
delma <sdy2856@gmail.com>
2020-02-04 15:58:51 +0900

    embed navigation controller

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -26,7 +26,8 @@ class YellowViewController: UIViewController {
     }
     
     @IBAction func closeButtonTouched(_ sender: Any){
-        self.dismiss(animated: true, completion: nil)
+        self.navigationController?.popViewController(animated: true)
+
     }
     
 
