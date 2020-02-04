9ba6189021c83f25e2c44009b78a2963007833a1
TTOzzi <devttozzi@gmail.com>
2020-02-04 17:50:02 +0900

    두번째 화면에 ImageView 추가
    contentMode 로 이미지가 ImageView 크기에 맞춰서 출력되도록 설정
    다음 버튼을 누를 때 마다 랜덤으로 이미지 출력

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.photoImageView.contentMode = .scaleToFill
+        self.photoImageView.image = UIImage(named: "01.jpg")
     }
-
-
+    
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+        let randNum = Int.random(in: 1...22)
+        
+        self.photoImageView.image = UIImage(named: randNum < 10 ? "0\(randNum).jpg" : "\(randNum).jpg")
+    }
+    
 }
 
