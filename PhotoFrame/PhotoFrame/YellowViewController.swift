993435781dfdf7dcba4369322d447f11cd010b2f
ehgud0670 <kimdo2297@gmail.com>
2020-02-04 16:57:24 +0900

    Feature : 노란색 창에 '닫기' 버튼을 누르면 화면이 닫아지게 기능 구현

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,20 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by kimdo2297 on 2020/02/04.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
