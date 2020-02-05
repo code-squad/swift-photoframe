cabf01a6b15980529e37c0f1f4ceb7750218b4fd
1Consumption <hanseop95@gmail.com>
2020-02-05 22:52:10 +0900

    readme 수정
    스크린샷 첨부
    second tap에 대한 변경 사항 정리

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -8,16 +8,47 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+extension SecondViewController : UIImagePickerControllerDelegate,
+UINavigationControllerDelegate{
+    func imagePickerController(_ picker:UIImagePickerController, didFinishPickingMediaWithInfo info : [String:Any]){}
+}
 
+class SecondViewController: UIViewController{
+    let picker = UIImagePickerController()
     @IBOutlet weak var photoImageView: UIImageView!
     @IBAction func nextImageButtonTouched(_ sender: Any) {
         let fileNumber = String(format: "%02d", Int.random(in: 1...22))
         self.photoImageView.image=UIImage(named:"\(fileNumber).jpg")
     }
     
+    @IBAction func selectButtonTouched(_ sender: Any) {
+        let alert =  UIAlertController(title: "사진 선택", message: "선택하세요", preferredStyle: .actionSheet)
+        let library =  UIAlertAction(title: "사진앨범", style: .default){ (action) in self.openPhotoLibrary()}
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(library)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.photoImageView.contentMode = .scaleAspectFill
+        self.photoImageView.layer.zPosition=1
+        picker.delegate=self
+        
     }
+    
+    func openPhotoLibrary(){
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            photoImageView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
