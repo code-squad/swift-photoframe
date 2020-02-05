4ece90b753511263d47a10b1a3ea0779ae030f7a
idevjinie <idevjinie@gmail.com>
2020-02-05 20:21:09 +0900

    IndigoViewController 화면 관련 콜백 함수 추가

--- PhotoFrame/PhotoFrame/IndigoViewController.swift
+++ PhotoFrame/PhotoFrame/IndigoViewController.swift
@@ -12,6 +12,23 @@ class IndigoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
     }
     
     @IBAction func closeButtonTouched(_ sender: Any) {
