b4532ab8a90dd69e0bd29ff79f27b73d3874f7af
idevjinie <idevjinie@gmail.com>
2020-02-05 19:58:56 +0900

    YellowViewController 화면에 추가한 버튼에 대한 IBAction 연결

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -16,7 +16,10 @@ class YellowViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
