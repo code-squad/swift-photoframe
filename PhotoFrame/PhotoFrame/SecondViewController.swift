bc9b9cc9dde5c71b34951c623f68fa5d8d791ac8
seizze <cwcwc3@gmail.com>
2020-02-05 11:36:19 +0900

    feat: Display selected photo from photo album

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -12,15 +12,48 @@ class SecondViewController: UIViewController {
 
     @IBOutlet weak var photoImageView: UIImageView!
     
+    private lazy var imagePicker = UIImagePickerController.photoAlbumImagePicker
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print(#file, #line, #function, #column)
+
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            imagePicker.delegate = self
+        }
     }
     
     @IBAction func nextImageButtonTouched(_ sender: Any) {
         let randomImageName = String(format: "%02d", Int.random(in: 1...22))
         self.photoImageView.image = UIImage(named: randomImageName)
     }
+    
+    @IBAction func selectButtonTouched(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
+extension SecondViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            photoImageView.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+private extension UIImagePickerController {
+    static var photoAlbumImagePicker: UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        return picker
+    }
+}
