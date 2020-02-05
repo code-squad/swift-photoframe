13a88d8d9b3c7f5096e723cec85932b5f24af12d
idevjinie <idevjinie@gmail.com>
2020-02-05 20:12:18 +0900

    IndigoViewController 파일 생성

---
+++ PhotoFrame/PhotoFrame/IndigoViewController.swift
@@ -0,0 +1,27 @@
+//
+//  IndigoViewController.swift
+//  PhotoFrame
+//
+//  Created by jinie on 2020/02/05.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class IndigoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
