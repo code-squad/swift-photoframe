b890e8e2009b3f9648c7e695c6d85315fb7821e6
1Consumption <hanseop95@gmail.com>
2020-02-05 10:23:43 +0900

    기능 추가
    second 스크린에 imageView를 띄워주고, 랜덤으로 사진 표시하게 함

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -10,21 +10,15 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+        let fileNumber = String(format: "%02d", Int.random(in: 1...22))
+        self.photoImageView.image=UIImage(named:"\(fileNumber).jpg")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.photoImageView.contentMode = .scaleAspectFit
+        self.photoImageView.clipsToBounds=true
     }
-    */
-
 }
