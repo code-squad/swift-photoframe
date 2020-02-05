4cd70f9ec87ef1c494533a3cf4941bd6b407c9f7
idevjinie <idevjinie@gmail.com>
2020-02-05 19:50:07 +0900

    YellowViewController 파일 생성

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by jinie on 2020/02/05.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
