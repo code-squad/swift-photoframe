4b2aede51772bf9f14197a10512518d3f0f2f28e
limwin94 <amuguna94@gmail.com>
2020-02-07 14:09:14 +0900

    ImageView 추가 및 아웃렛으로 연결

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
