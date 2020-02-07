85dbe402bac3d302c6388a1e41a168ec0d3e28d9
MagnaPax <MagnaPax@outlook.kr>
2020-02-07 15:16:31 +0900

    Add a new ViewController class named BlueViewController on the project and add 닫기button on it

---
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -0,0 +1,33 @@
+//
+//  BlueViewController.swift
+//  PhotoFrame
+//
+//  Created by 한천희 on 2020/02/07.
+//  Copyright © 2020 한천희. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
