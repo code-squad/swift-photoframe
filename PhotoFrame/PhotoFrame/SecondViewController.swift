0bb5331b8fd0a7c3f4ec5b7d97a0adc265de3cc2
MagnaPax <MagnaPax@outlook.kr>
2020-02-07 20:43:06 +0900

    Place UIImageView on the Second Scene

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class SecondViewController: UIViewController {
 
     
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
