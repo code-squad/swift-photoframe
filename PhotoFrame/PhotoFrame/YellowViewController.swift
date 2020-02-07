61fbb878ea64a7c8403f1575e62d15874a316c42
MagnaPax <MagnaPax@outlook.kr>
2020-02-07 14:31:17 +0900

    Add new ViewController Class on the project named YellowViewController

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by 한천희 on 2020/02/07.
+//  Copyright © 2020 한천희. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
