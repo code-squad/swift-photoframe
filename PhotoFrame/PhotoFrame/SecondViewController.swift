5e1aa786d967c6a0e7adbaf7f0fc19613ccce2ea
idevjinie <idevjinie@gmail.com>
2020-02-08 12:30:52 +0900

    photoImageView 아웃렛 연결

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -15,6 +15,7 @@ class SecondViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var photoImageView: UIImageView!
+    
 }
 
