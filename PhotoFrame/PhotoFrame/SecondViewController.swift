ca17c6192a9b6505b3c0fd73da84438e5db5654e
limwin94 <amuguna94@gmail.com>
2020-02-09 23:15:28 +0900

    UIImagePickerController delegate 구현, 버튼 구현

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 class SecondViewController: UIViewController {
 
     @IBOutlet weak var photoImageView: UIImageView!
+    let uiPicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //picker에 대한 delegate 설정
+        self.uiPicker.delegate = self
     }
 
     @IBAction func nextImageButtonTouched(_ sender: UIButton) {
@@ -23,7 +26,10 @@ class SecondViewController: UIViewController {
     }
     
     @IBAction func selectButtonTouched(sender: UIButton){
-        
+         //picker가 어떤 컨텐츠를 선택하는지 설정(사진앨범)
+         self.uiPicker.sourceType = .photoLibrary
+         //새로운 viewController로 전달, 모달로 화면 전환
+         self.present(self.uiPicker, animated: true)
     }
     
     func randImg() -> String {
@@ -31,6 +37,25 @@ class SecondViewController: UIViewController {
         let img = String(format: "%02d", imgNum)
         
         return img
+    }
+}
+
+//해당 클래스는 2개의 protocol를 따른다.
+extension SecondViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //UIImagePickerControllerDelegate의 함수를 가져옴
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        //조건부 다운캐스팅, 타입을 확인하는 용도, 캐스팅 하려는 타입에 부합하지 않는다면 nil을 반환할 것이다.
+        //as!로 강제 다운 캐스팅을 할시 위와 같이 nil을 반환하는것이 아닌 런타임 에러가 발생할것(지양해야함)
+        if let img = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            print(img)
+            photoImageView.image = img
+            
+        }
+        //ImageView에 넣어주고 modal을 닫아줌.
+        self.dismiss(animated: true)
+        
     }
 }
 
