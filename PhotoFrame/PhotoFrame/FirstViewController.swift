eb1bad02fcc094aff958feec0509865ae282ecff
zombietux <>
2021-02-08 20:53:23 +0900

    Viewcontroller이름 변경

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  PhotoFrame
 //
 //  Created by zombietux on 2021/02/08.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
