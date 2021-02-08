e279a3e6efa62390a1303a2bea66d7e7fed7690b
okstring <milkoi7801@gmail.com>
2021-02-08 18:08:37 +0900

    Create PhotoFrame
    Make UITabBarController
     - Select initial View Controller
     - Print #file, #line, #function, #column

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by Issac on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
