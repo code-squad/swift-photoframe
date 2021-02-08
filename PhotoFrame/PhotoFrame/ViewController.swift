62e4a96f01359012e7ebc638b56066b27d0c7cd5
HongzCloud <jihong21511@gmail.com>
2021-02-08 23:10:56 +0900

    ✨ Complete Tabbed App
    -TabBarController를 추가하고 Initial ViewController로 지정 \n -기존 UIViewController 제거 \n -에  추가 후 실행

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -13,6 +13,7 @@ class MyUITabBarController: UITabBarController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print(#file, #line, #function, #column)
+        print("미션완료!")
     }
 }
 
