7542e72828840817e237850258efdedee86c4432
Jung Kim <godrm77@gmail.com>
2021-02-08 20:54:31 +0900

    Revert "UITabBarController 추가, 콘솔 출력"
    This reverts commit 8b3d25d382eb9c16713867468d7dc00aef60c23b.

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UITabBarController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(#file, #line, #function, #column)
+        // Do any additional setup after loading the view.
     }
 
 
