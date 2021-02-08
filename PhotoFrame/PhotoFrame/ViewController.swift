e86c4e08b1f61b9d1b929980c880acca7dbe13c1
Sonjh1306 <yale1306@gmail.com>
2021-02-08 22:39:52 +0900

    Complete Tabbed

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by sonjuhyeong on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+
+
+}
+
