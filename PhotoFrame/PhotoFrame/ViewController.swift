b030ea247d54c1d2b05c4ebb8897bd1f5ed7311a
zeke <yangsin1031@naver.com>
2021-02-08 22:58:23 +0900

    XcodeProject 생성 및 TabBarController 생성
    Xcode를 새로 생성하고 탭바 컨트롤러 생성하고 기존 뷰컨트롤러 삭제

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by 양준혁 on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
