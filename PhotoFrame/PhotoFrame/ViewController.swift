a3f048ca58b0680f3fa91e46883c938000785bbe
zombietux <>
2021-02-08 18:44:04 +0900

    탭바 컨트롤러 추가

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by zombietux on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
