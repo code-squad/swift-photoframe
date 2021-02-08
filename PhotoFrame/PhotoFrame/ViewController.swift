d819a1a798165efff6f2f21989b4bb937d24a884
Pico <miknuhij@gmail.com>
2021-02-08 21:30:13 +0900

    Tabbed 앱

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by Jihun Kim on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        
+        print(#file, #line, #function, #column)
+        
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
