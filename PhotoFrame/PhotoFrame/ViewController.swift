4093a9104b4817ad857f986b3509794de149028c
HongzCloud <jihong21511@gmail.com>
2021-02-08 20:48:28 +0900

    🎉 Create Tabbed App project

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by 오킹 on 2021/02/08.
+//
+
+import UIKit
+
+class MyUITabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
+    }
+}
+
