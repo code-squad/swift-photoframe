7e73fa9e2cf1f13e98d7ce6271afcd42eb30d7de
lena <sue822330@gmail.com>
2021-02-08 17:58:00 +0900

    step1 - embed in tab bar controller

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by Ador on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(#file, #line, #function, #column)
+    }
+
+
+}
+
