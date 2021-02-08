75f7ad396c268703bdfd08616537ce941a29db3b
Ray <readingod@naver.com>
2021-02-08 17:31:46 +0900

    Add PhotoFrame Step1 Add Tab Bar Controller Embeded in View Controller

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by 서우석 on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+
+
+}
+
