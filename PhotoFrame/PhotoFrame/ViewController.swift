56bcdeb003ffae46f9bf62f4170597ea9d0dd5ee
BMO <b38238496@gmail.com>
2021-02-08 23:25:23 +0900

    Complete Tabbed

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by user on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
+    }
+
+
+}
+
