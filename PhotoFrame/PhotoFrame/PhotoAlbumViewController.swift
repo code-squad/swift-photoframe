2270ad7ccd54a4b7da5b2bb5745986edb5f36ef6
hoonha choi <sosqsz2@naver.com>
2021-02-09 18:30:41 +0900

    Int String 변환 코드 수정, 애니메니션 추가

--- PhotoFrame/PhotoFrame/PhotoAlbumViewController.swift
+++ PhotoFrame/PhotoFrame/PhotoAlbumViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 class PhotoAlbumViewController: UIViewController {
 
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoAlbumLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func nextImageButtonTouched(_ sender: Any) {
-        self.photoImageView.image = UIImage(named: randomImageSelect())
+        imageTransitionAnimate()
+        labelAlpha()
     }
 }
 
@@ -24,6 +26,21 @@ extension PhotoAlbumViewController {
     
     func randomImageSelect() -> String {
         let number = Int.random(in: 1...22)
-        return number < 10 ? "0\(number).jpg" : "\(number).jpg"
+        return String(format: "%02d.jpg", number)
+    }
+    
+    func imageTransitionAnimate() {
+        UIView.transition(with: self.photoImageView,
+                          duration: 1.0,
+                          options: .transitionCrossDissolve,
+                          animations: {
+                            self.photoImageView.image = UIImage(named: self.randomImageSelect())
+                          }, completion: nil)
+    }
+    
+    func labelAlpha() {
+        UIView.animate(withDuration: 0.5) {
+            self.photoAlbumLabel.alpha = 1
+        }
     }
 }
