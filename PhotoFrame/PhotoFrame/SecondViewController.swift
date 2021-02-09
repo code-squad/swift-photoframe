e903ead7c5a4cbd8cc7f72a08461baeaabe99bd7
BMO <b38238496@gmail.com>
2021-02-09 19:40:36 +0900

    SecondView 단순 추가

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by user on 2021/02/09.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
