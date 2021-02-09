2fd1ae7c975059a86e2a871ea3cb5524822665c1
Jisoo Kim <ghis22130@naver.com>
2021-02-09 09:27:57 +0900

    Add Step01
    Dumba-step01 브랜치를 새로 생성하였습니다.
    PhotoFrame프로젝트를 생성하였고 기존 ViewController를 지우고 TabBar Controller를 새로 만든 뒤 Class와 연결시켜주었습니다.
    print 요구사항 작성 후 콘솔 창 변화를 확인해보았습니다.

---
+++ PhotoFrame/PhotoFrame/TabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by 지북 on 2021/02/09.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file,#function,#line,#column)
+    }
+
+
+}
+
