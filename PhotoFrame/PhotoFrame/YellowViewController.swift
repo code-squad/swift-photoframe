2620eeacb898bb5dbc67855f249fb72fe57aeb1c
zeke <yangsin1031@naver.com>
2021-02-09 17:17:33 +0900

    화면관련 콜백 함수 추가
    화면관련 콜백 함수들을 확인하기위해 YellowScene에 추가하여 화면의 흐름들을 확인

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -11,7 +11,18 @@ class YellowViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
     }
     @IBAction func closeButtonTouched(_ sender: UIButton) {
         self.dismiss(animated: true)
