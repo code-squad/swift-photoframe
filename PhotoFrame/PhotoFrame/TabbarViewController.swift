fe8ce8b10d344ee902c7ec5deaea8fa2e6ca5e94
홍승표 <69688078+TChaper@users.noreply.github.com>
2021-02-09 16:37:07 +0900

    add: TabbarViewController

---
+++ PhotoFrame/PhotoFrame/TabbarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TabbarViewController.swift
+//  PhotoFrame
+//
+//  Created by 홍승표 on 2021/02/09.
+//
+
+import UIKit
+
+class TabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      print(#file, #line, #function, #column)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
