ea57726ce52dcf5249984e40a43da5b5d7e6ad18
HongzCloud <jihong21511@gmail.com>
2021-02-09 17:12:58 +0900

    💄 Update Label attributes on FirstViewController
    FirstViewController에서 firstLabel,firstLabelDescription 의 속성들을 변경했다.

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -9,8 +9,21 @@ import Foundation
 
 class FirstViewController: UIViewController {
     @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
     
     override func viewDidLoad() {
         self.firstLabel.text = "Hong의 사진"
+        self.firstDescription.text = "Hong의 사진입니다"
+        
+        //text 부분적으로 색상 변화
+        let fontAttribute = NSMutableAttributedString(string: firstLabel.text!)
+        fontAttribute.addAttribute(.foregroundColor, value: UIColor.red, range: (firstLabel.text! as NSString).range(of: "Hong"))
+        fontAttribute.addAttribute(.foregroundColor, value: UIColor.blue, range: (firstLabel.text! as NSString).range(of: "사진"))
+        
+        self.firstLabel.backgroundColor = UIColor.green
+        self.firstLabel.alpha = 0.5
+        self.firstLabel.font = .systemFont(ofSize: 60)
+        self.firstLabel.attributedText = fontAttribute
+        self.firstDescription.alpha = 0.5
     }
 }
