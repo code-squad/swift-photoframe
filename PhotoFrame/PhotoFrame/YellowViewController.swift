6136d5a8d7fadeb378a9d00776ddef1b1df7035e
zombietux <>
2021-02-09 14:19:43 +0900

    FirstViewController NavigationController추가 후 닫기 IBAction 수정

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -30,6 +30,6 @@ class YellowViewController: UIViewController {
     }
     
     @IBAction func closeButtonTouched(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+        self.navigationController?.popViewController(animated: true)
     }
 }
