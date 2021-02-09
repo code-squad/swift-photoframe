d3e9db214ff8d5060dfdfaadaea8c946bd562196
HongzCloud <jihong21511@gmail.com>
2021-02-09 16:15:49 +0900

    💄 Add firstLabel to FirstViewController
    FirstViewController에 firstLabel을 만들고 IBOutlet으로 연결했다.

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by 오킹 on 2021/02/09.
+//
+import UIKit
+import Foundation
+
+class FirstViewController: UIViewController {
+    @IBOutlet weak var firstLabel: UILabel!
+}
