68184f71e6530049406ffb5f34e5505c2483b68a
zombietux <unknown>
2021-02-09 15:07:15 +0900

    PhotoVC Second Scene에 연결 후 IBOutlet, IBAction 연결

---
+++ PhotoFrame/PhotoFrame/PhotoAlbumViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoAlbumViewController.swift
+//  PhotoFrame
+//
+//  Created by zombietux on 2021/02/09.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+    }
+}
