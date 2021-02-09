63c7937ce92a8b6d7c9bb8ebe5c3b284c4d4316b
Lia <judykim257@gmail.com>
2021-02-09 17:37:49 +0900

    Step4. new Scene 추가 및 색 구현
    새로운 ViewController를 추가하여 새 Scene 을 만듦
    Segue를 사용해서 두 scene 연결함

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,29 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by Lia on 2021/02/09.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemYellow
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
