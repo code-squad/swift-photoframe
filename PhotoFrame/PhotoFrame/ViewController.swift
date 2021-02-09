5782cfd92bb082ec979519b8204b365c3aa279ab
shimyoungmin <dudals6965@gmail.com>
2021-02-09 12:45:37 +0900

    Mission 1.2 UILabel & IBOutlet Create

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -9,10 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    @IBOutlet weak var firstDescription: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print(#file, #line, #function, #column)
+        self.firstLabel.text = "JK의 사진액자"
+        self.firstLabel.font = UIFont.systemFont(ofSize: 30)
+        self.firstLabel.textColor = UIColor.purple
+        self.firstLabel.backgroundColor = UIColor.darkGray
+        
+        self.firstDescription.text = "Loaded by FirstViewController"
+        let font = UIFont(name: "Apple Color Emoji", size: 50)
+        
+        var attributeStr = NSMutableAttributedString(string: firstDescription.text!)
+        attributeStr.addAttribute(.font, value: font, range: (firstDescription.text as! NSString).range(of: "First"))
+        attributeStr.addAttribute(.foregroundColor, value: UIColor.blue, range: (firstDescription.text as! NSString).range(of: "First"))
+        attributeStr.addAttribute(.backgroundColor, value: UIColor.red, range: (firstDescription.text as! NSString).range(of: "ller"))
+        
+        firstDescription.attributedText = attributeStr
     }
 
 
