2ab74bb7412b56ae6c6b74d2faa732dbc5e47733
hoonha choi <sosqsz2@naver.com>
2021-02-09 14:03:11 +0900

    animation 연습

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 class YellowViewController: UIViewController {
 
     @IBOutlet weak var closeBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
     
     @IBOutlet weak var closeBtnBottom: NSLayoutConstraint!
+    @IBOutlet weak var closeBtnCenterX: NSLayoutConstraint!
     
+    @IBOutlet weak var nextBtnCenterY: NSLayoutConstraint!
+    @IBOutlet weak var nextBtnCenterX: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +29,7 @@ class YellowViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        animation()
+        closeBtnUpAnimation()
         print(#file, #line, #function, #column)
     }
     
@@ -40,14 +44,53 @@ class YellowViewController: UIViewController {
     }
     
     @IBAction func closeButtonTouched(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
+        viewCloseAnimation()
     }
     
-    func animation() {
-        // 최근에 공부한 애니메이션을 적용해보겠습니다
+}
+
+
+extension YellowViewController {
+    
+    func closeBtnUpAnimation() {
         closeBtnBottom.constant = self.view.frame.height/2 - 100
-        UIView.animate(withDuration: 1) {
+        UIView.animate(withDuration: 0.5) {
             self.view.layoutIfNeeded()
         }
+    }
+    
+    func viewCloseAnimation() {
+        UIView.animateKeyframes(withDuration: 1, delay: 0, options: []) {
+            self.leftAnimation(self.nextBtnCenterX)
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.25) {
+                self.view.layoutIfNeeded()
+            }
+            self.upAnimation(self.nextBtnCenterY)
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
+                self.zeroAlphaView(self.nextBtn)
+                self.view.layoutIfNeeded()
+            }
+            self.leftAnimation(self.closeBtnCenterX)
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.25) {
+                self.view.layoutIfNeeded()
+            }
+            self.upAnimation(self.closeBtnBottom)
+            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25) {
+                self.zeroAlphaView(self.closeBtn)
+                self.view.layoutIfNeeded()
+            }
+        } completion: { (_) in
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func leftAnimation(_ constraint : NSLayoutConstraint) {
+        constraint.constant = -(self.view.frame.width/3)
+    }
+    func upAnimation(_ constraint : NSLayoutConstraint) {
+        constraint.constant = -150
+    }
+    func zeroAlphaView(_ ui : UIView) {
+        ui.alpha = 0
     }
 }
