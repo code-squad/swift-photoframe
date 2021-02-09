02a09175bea723e6a4632636b60bf763cccb6eb4
Song Lee <songlish233@gmail.com>
2021-02-09 16:20:31 +0900

    추가 VC 생성, 직전 생성한 Scene의 다음 버튼 통해 연결

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by Song on 2021/02/09.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func nextButtonTouched(_ sender: Any) {
+    }
+}
