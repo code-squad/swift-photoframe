de4402198e7b416e9d8689ff39846e2ee117dd0c
hoonha choi <sosqsz2@naver.com>
2021-02-09 15:28:05 +0900

    blueViewController 생성 닫기 버튼 생성

---
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BlueViewController.swift
+//  PhotoFrame
+//
+//  Created by HOONHA CHOI on 2021/02/09.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
