39b0e5cb5c585d043ecf051181c40d121837d53b
zeke <yangsin1031@naver.com>
2021-02-09 16:35:47 +0900

    YellowViewController 생성
    YellowViewController를 생성한 후 연결

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,17 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by 양준혁 on 2021/02/09.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
