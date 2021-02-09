eee56ad0bb6748b5d41fc93f45909995392cf5ca
Ray <readingod@naver.com>
2021-02-09 12:22:09 +0900

    Add UILabel Code Add firstLabel and firstDescription Variables Study UILabel Properties Font, Color, Background Color

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -8,9 +8,17 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.firstLabel.text = "Ray의 사진액자"
+        self.firstDescription.text = "글자 꾸미기"
+        self.firstDescription.textColor = UIColor.white
+        self.firstDescription.backgroundColor = UIColor.blue
+        self.firstDescription.font = UIFont.boldSystemFont(ofSize: 15)
         print(#file, #line, #function, #column)
     }
 
