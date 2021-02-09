bce5ab85bb6525d8ebf1b0e62bbca838fcac98d4
Lia <judykim257@gmail.com>
2021-02-09 18:15:05 +0900

    Step5. ViewController 연결 및 화면 닫기 동작
    step 5. 에서 새로운 ViewController를 추가하는지 모르고 전 스텝에서 이미 했었다
    새로운 것은 화면 닫기 동작인 dismiss

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -15,15 +15,8 @@ class YellowViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func closeButtonTouch(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
