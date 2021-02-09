fb68ad6d92e955d58178af582fbfafd5a7d4043c
hoonha choi <sosqsz2@naver.com>
2021-02-09 15:33:11 +0900

    blueViewController closeButton 연결 닫기액션 구현

--- PhotoFrame/PhotoFrame/BlueViewController.swift
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -9,10 +9,36 @@ import UIKit
 
 class BlueViewController: UIViewController {
 
+    @IBOutlet weak var closeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+}
 
+extension BlueViewController {
+    
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
 }
