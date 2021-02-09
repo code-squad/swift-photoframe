e70c91f53735bbe84647b0fdc8f0c7e1967717af
jhpark-steve <park9368@naver.com>
2021-02-09 12:43:32 +0900

    step-2 완료 하였습니다. 레이블 만들어서 속성값 변경했습니다.
    근데 기기마다 레이블이 보이는 위치마다 다를까봐 큰 글씨(photo Label)에는 constraints에 centerX 주고 centerY 주고
    작은 글씨(First description) 에는 centerX, centerY, first description.top = photo label.bottom + 35 추가 했더니
    Label 아예 표시가 되지 않고 에러가 뜨네요ㅠ Label이 겹쳐서 그런건지 정확한 원인을 못 찾겠습니다.

---
+++ PhotoFrame/PhotoFrame/Tab1ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  Tab1ViewController.swift
+//  PhotoFrame
+//
+//  Created by 박정하 on 2021/02/09.
+//
+
+import UIKit
+
+class Tab1ViewController : UIViewController{
+    
+    @IBOutlet weak var photoLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
+    
+    let FontSIZE : CGFloat = 35
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        changephotoLabelelement()
+        changeDescriptionLabelelement()
+        
+    }
+    
+    func changephotoLabelelement() -> Void{
+        self.photoLabel.text = "ZG의 사진액자"
+        self.photoLabel.textColor = .systemBackground
+        self.photoLabel.backgroundColor = .systemRed
+        self.photoLabel.alpha = 0.3
+        self.photoLabel.font = .systemFont(ofSize: 52)
+    }
+    
+    func changeDescriptionLabelelement() -> Void{
+        self.firstDescription.text = "로드 됨 퍼스트 뷰 컨트롤러에 의해"
+        self.firstDescription.textColor = .systemRed
+        self.firstDescription.backgroundColor = .label
+        self.firstDescription.alpha = 0.5
+        self.firstDescription.font = .systemFont(ofSize: 15)
+    }
+}
