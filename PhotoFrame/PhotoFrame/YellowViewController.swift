d33548dac991833757c1dd04798b1faef03fee05
hoonha choi <sosqsz2@naver.com>
2021-02-09 12:01:17 +0900

    yelloViewcontroller create Lifecycle write

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,35 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by HOONHA CHOI on 2021/02/09.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+}
