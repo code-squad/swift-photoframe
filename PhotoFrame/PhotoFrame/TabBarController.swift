74c1f1a8358be37c6e40f2cd318827b86d1f3a1e
jhpark-steve <park9368@naver.com>
2021-02-09 11:16:27 +0900

    Step1 진행하고 print 실행했습니다
    Step1 진행하고 print문 출력했습니다
    정상적으로 출력되는 것 확인했고 탭 컨트롤러와 탭바에 대해 학습하였습니다

---
+++ PhotoFrame/PhotoFrame/TabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by 박정하 on 2021/02/09.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+    }
+}
