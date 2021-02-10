3144fe5b22d3bd1507adddbb447894ecb08e781b
HongzCloud <jihong21511@gmail.com>
2021-02-10 16:24:19 +0900

    ✨ Create NextOfFristViewController Class
    NextOfFristViewController 라는 커스텀 클래스를 생성하고 인터페이스 빌더 화면과 연결했다.

---
+++ PhotoFrame/PhotoFrame/NextOfFirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NextOfFirstViewController.swift
+//  PhotoFrame
+//
+//  Created by 오킹 on 2021/02/10.
+//
+
+import UIKit
+
+class NextOfFirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
