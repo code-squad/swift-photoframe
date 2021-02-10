9e46b4eb4b2b6cf78da40e8c174035b04a2577d1
dahun-lee-daji <ssss6596@gmail.com>
2021-02-10 14:41:40 +0900

    탭바 아이콘 변경

--- PhotoFrame/PhotoFrame/InitialViewController.swift
+++ PhotoFrame/PhotoFrame/InitialViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hello tab bar")
         // Do any additional setup after loading the view.
     }
 
