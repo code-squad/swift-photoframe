2b9bb644947fb983d99fef90ab05efb1ef75e9b6
HongzCloud <jihong21511@gmail.com>
2021-02-10 18:37:24 +0900

    ✨ Add callback function to NextOfFirstViewController
    NextOfFirstView와 NextNextOfFristView를 비교하기 위해 NextOfFirstViewController에도 5가지 콜백함수에 print(#file, #line, #function, #column) 코드를 추가했다.

--- PhotoFrame/PhotoFrame/NextOfFirstViewController.swift
+++ PhotoFrame/PhotoFrame/NextOfFirstViewController.swift
@@ -11,22 +11,25 @@ class NextOfFirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(#file, #line, #function, #column)
         // Do any additional setup after loading the view.
     }
     
     @IBAction func closeButtonTouched(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //will: 직전
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
     }
-    */
-
+    //Did: 직후
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
 }
