f8e8bdcbb9532ea029d3430739e999520c64ebbe
Song Lee <songlish233@gmail.com>
2021-02-10 14:36:29 +0900

    IndigoViewController 연결 후 dismiss 버튼 생성

---
+++ PhotoFrame/PhotoFrame/IndigoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  IndigoViewController.swift
+//  PhotoFrame
+//
+//  Created by Song on 2021/02/10.
+//
+
+import UIKit
+
+class IndigoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
