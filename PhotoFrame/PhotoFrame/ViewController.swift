25ecd82bed71c121635ee657992d12832c674c3e
Song Lee <songlish233@gmail.com>
2021-02-10 15:09:24 +0900

    VC 사이클에 따라 콘솔 메시지 출력

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -13,7 +13,26 @@ class ViewController: UITabBarController {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
 
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
 
 }
 
