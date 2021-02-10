bd671d526e93afe55f40e7d8bab68a796eac44c7
HongzCloud <jihong21511@gmail.com>
2021-02-10 16:31:58 +0900

    💄 Add closeButtonTouched to NextOfFirstView
    NextOfFristView에서 이전으로 돌아가는 기능을 가진 버튼을 추가시켰다.

--- PhotoFrame/PhotoFrame/NextOfFirstViewController.swift
+++ PhotoFrame/PhotoFrame/NextOfFirstViewController.swift
@@ -15,7 +15,10 @@ class NextOfFirstViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
