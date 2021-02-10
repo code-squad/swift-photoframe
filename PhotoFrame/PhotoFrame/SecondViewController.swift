f3ea9d05e6b7a39b5ad78961cf71eb75d61a0620
zeke <yangsin1031@naver.com>
2021-02-10 15:38:05 +0900

    랜덤 이미지 표시
    랜덤으로 이미지 표시를 위해 numbers 배열을 만들어 1~22를 넣어주고 랜덤으로 숫자를 반환하여 이미지 표시

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    var numbers = ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22"]
     @IBOutlet var photoImageView: UIImageView!
     
     override func viewDidLoad() {
@@ -16,6 +17,7 @@ class SecondViewController: UIViewController {
 
     }
     @IBAction func nextImageButtonTouched(_ sender: UIButton) {
+        self.photoImageView.image = UIImage(named: "\(numbers.randomElement()!).jpg")
     }
     
 }
