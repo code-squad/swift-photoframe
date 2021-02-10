76ef3eda4469636d4fab1e1303db562a34bea15e
HongzCloud <jihong21511@gmail.com>
2021-02-10 16:38:42 +0900

    💄 Add closeButtonTouched to NextNextOfFristView
    NextNextOfFirstViewController 라는 커스텀 클래스를 생성하고 인터페이스 빌더 화면과 연결 했다. 그리고 NextNextOfFristView에서 이전으로 돌아가는 기능을 가진 버튼을 추가시켰다.

---
+++ PhotoFrame/PhotoFrame/NextNextOfFristViewController.swift
@@ -0,0 +1,32 @@
+//
+//  NextNextOfFristViewController.swift
+//  PhotoFrame
+//
+//  Created by 오킹 on 2021/02/10.
+//
+
+import UIKit
+
+class NextNextOfFristViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
