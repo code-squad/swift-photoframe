80b5ca49f677195f0540708fdf75976b944a039b
Jisoo Kim <ghis22130@naver.com>
2021-02-10 15:02:53 +0900

    ViewController LifeCycle 구현
    각 ViewController에 콜백 함수에 print함수를 추가하였습니다

--- PhotoFrame/PhotoFrame/GreenViewController.swift
+++ PhotoFrame/PhotoFrame/GreenViewController.swift
@@ -20,15 +20,19 @@ class GreenViewController: UIViewController {
         print(#file,#function,#line,#column)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file,#function,#line,#column)
     }
-    */
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file,#function,#line,#column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file,#function,#line,#column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file,#function,#line,#column)
+    }
 }
