d5cbb0e1d63d3bf49b6a3790982238bb3af6bc56
Song Lee <songlish233@gmail.com>
2021-02-10 15:28:26 +0900

    리팩터링

--- PhotoFrame/PhotoFrame/WhiteViewController.swift
+++ PhotoFrame/PhotoFrame/WhiteViewController.swift
@@ -12,18 +12,9 @@ class WhiteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
-        self.photoLabel.text = "Lollo's Photo"
-        self.photoLabel.adjustsFontSizeToFitWidth = true
-        self.photoLabel.shadowColor = .lightGray
-        self.photoLabel.shadowOffset = CGSize(width: 0.5, height: 0.5)
-        
-        self.photoDescription.text = "착한 사람 눈에만 보이는 사진입니다"
-        self.photoDescription.adjustsFontSizeToFitWidth = true
-        self.photoDescription.textColor = .white
-        self.photoDescription.backgroundColor = .black
-        self.photoDescription.alpha = 0.6
+        setLabels()
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print(#file, #line, #function, #column)
@@ -48,9 +39,7 @@ class WhiteViewController: UIViewController {
     @IBOutlet weak var photoDescription: UILabel!
     
     @IBAction func nextButtonTouchedUpInside(_ sender: Any) {
-        self.photoLabel.textColor = .black
-        self.photoLabel.backgroundColor = .clear
-        self.photoLabel.alpha = 1
+        photoLabelToDefault()
         
         let mainStoryBoard = UIStoryboard(name: "Main", bundle: nil)
         let nextVC = mainStoryBoard.instantiateViewController(withIdentifier: "YellowVC")
@@ -58,9 +47,35 @@ class WhiteViewController: UIViewController {
     }
     
     @IBAction func nextButtonTouchedDown(_ sender: Any) {
+        photoLabelToPurple()
+    }
+}
+
+//MARK: - Change Label
+extension WhiteViewController {
+    
+    func setLabels() {
+        self.photoLabel.text = "Lollo's Photo"
+        self.photoLabel.adjustsFontSizeToFitWidth = true
+        self.photoLabel.shadowColor = .lightGray
+        self.photoLabel.shadowOffset = CGSize(width: 0.5, height: 0.5)
+        
+        self.photoDescription.text = "착한 사람 눈에만 보이는 사진입니다"
+        self.photoDescription.adjustsFontSizeToFitWidth = true
+        self.photoDescription.textColor = .white
+        self.photoDescription.backgroundColor = .black
+        self.photoDescription.alpha = 0.6
+    }
+    
+    func photoLabelToDefault() {
+        self.photoLabel.textColor = .black
+        self.photoLabel.backgroundColor = .clear
+        self.photoLabel.alpha = 1
+    }
+    
+    func photoLabelToPurple() {
         self.photoLabel.textColor = .purple
         self.photoLabel.backgroundColor = .lightGray
         self.photoLabel.alpha = 0.5
     }
 }
-
