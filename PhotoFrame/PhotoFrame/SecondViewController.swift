57370b957f98e2284354ab92974c800e152ca608
zeke <yangsin1031@naver.com>
2021-02-10 14:29:40 +0900

    SecondViewController 생성
    SecondViewController 생성 후 스토리보드의 SecondScene과 연결

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecongViewController.swift
+//  PhotoFrame
+//
+//  Created by 양준혁 on 2021/02/10.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
