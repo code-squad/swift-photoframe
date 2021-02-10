5081518353d9df4a488dc580f28197a43be8bcfa
MagnaPax <chunhee.han@outlook.com>
2021-02-10 22:45:06 +0900

    feat : 리터럴 표현으로 정보 표시
    Literal Expression 사용하여 print 메소드 실행되는 곳의 정보 표시

--- photoframe/photoframe/PhotoFrameViewController.swift
+++ photoframe/photoframe/PhotoFrameViewController.swift
@@ -11,9 +11,6 @@ class PhotoFrameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("\(#file)\n\(#line)\n\(#function)\n\(#column)")
     }
-
-
 }
-
