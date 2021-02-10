f8ba963a76e155cda5a2f24a111f6a2994f99c4a
HongzCloud <jihong21511@gmail.com>
2021-02-10 16:59:50 +0900

    ✨ Add callback function to NextNextOfFirstViewController
    NextNextOfFirstViewController의 5가지(viewWillApper,viewDidApper,viewWillDisappear,viewDidDisappear,ViewDidLoad) 콜백함수에 print(#file, #line, #function, #column) 코드를 추가 했다.

--- PhotoFrame/PhotoFrame/NextNextOfFristViewController.swift
+++ PhotoFrame/PhotoFrame/NextNextOfFristViewController.swift
@@ -11,14 +11,27 @@ class NextNextOfFristViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(#file, #line, #function, #column)
         // Do any additional setup after loading the view.
     }
     
     @IBAction func closeButtonTouched(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    
+    //will: 직전
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    //Did: 직후
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
     /*
     // MARK: - Navigation
 
