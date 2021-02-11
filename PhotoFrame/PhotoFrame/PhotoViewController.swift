2734e78ad8a820c5613f1cd9ae0f0cac8d6b080b
Song Lee <songlish233@gmail.com>
2021-02-11 14:28:04 +0900

    View 로드/🎲 터치 시 이미지가 랜덤으로 나타나도록 구현

--- PhotoFrame/PhotoFrame/PhotoViewController.swift
+++ PhotoFrame/PhotoFrame/PhotoViewController.swift
@@ -10,12 +10,26 @@ import UIKit
 class PhotoViewController: UIViewController {
 
     @IBOutlet weak var photoLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         photoLabel.text = "Photo Album"
+        photoLabel.adjustsFontSizeToFitWidth = true
+        setRandomImage()
         super.viewDidLoad()
     }
 
     @IBAction func randomButtonTouched(_ sender: Any) {
+        setRandomImage()
+    }
+    
+    func generateImageNumber() -> String {
+        let randomNumber = Int.random(in: 1...22)
+        return randomNumber > 9 ? "\(randomNumber)" : "0\(randomNumber)"
+    }
+    
+    func setRandomImage() {
+        let imageNumber = generateImageNumber()
+        self.photoImageView.image = UIImage(named: "Demo Images/\(imageNumber).jpg")
     }
 }
