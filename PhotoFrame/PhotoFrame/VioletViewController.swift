197f6876f203c9025eb9f32b03e0025d0f4a52cc
jhpark-steve <park9368@naver.com>
2021-02-11 20:39:07 +0900

    화면 등장, 사라짐 관련 함수 추가 했습니다.
    화면 등장 사라지는 함수 추가하고 관련 내용 학습했습니다. 특이사항은 없는 것 같네요.

---
+++ PhotoFrame/PhotoFrame/VioletViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PinkViewController.swift
+//  PhotoFrame
+//
+//  Created by 박정하 on 2021/02/11.
+//
+
+import UIKit
+
+class VioletViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
