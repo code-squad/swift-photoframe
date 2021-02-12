bb0027854dcca4c15167eb7121411030ca5707e4
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-02-12 21:39:24 +0900

    Add view controller life cycle test mothods

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -13,9 +13,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(#file, #line, #function, #column)
         self.photoLabel.text = "JEJE의 사진액자"
         self.firstDescription.text = "Are you ready to make?"
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(false)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(false)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(false)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(false)
+        print(#file, #line, #function, #column)
+    }
+    
+    
     @IBAction func nextButtonTouched(_ sender: UIButton) {
             self.photoLabel.textColor = UIColor.blue
             self.photoLabel.backgroundColor = UIColor.yellow
