6f44828626c8132899e0f6163e72f4cbdce59dd0
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-02-12 20:54:39 +0900

    Add BlueViewController and close button on it

---
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BlueViewController.swift
+//  PhotoFrame
+//
+//  Created by 조중윤 on 2021/02/12.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func CloseButtonTouched(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
