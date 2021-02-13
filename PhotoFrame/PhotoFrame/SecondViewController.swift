47b4dbefb14a53c10ae8a438a2336718c5231756
BMO <b38238496@gmail.com>
2021-02-13 21:10:17 +0900

    SecondViewController에 랜덤 이미지 생성 버튼 추가
    scaleAspectFill로 버튼을 눌렀을 때 생성되는 이미지의 비율은 유지되면서 크기가 조정됨.

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -14,5 +14,14 @@ class SecondViewController: UIViewController{
         // Do any additional setup after loading the view.
     }
     
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+        let randNum = String(format: "%02d" ,Int.random(in: 1...22))
+        let imageName = "Demo Images/\(randNum).jpg"
+        self.photoImageView.contentMode = .scaleAspectFill
+        self.photoImageView.image = UIImage(named: imageName)
+    }
 }
 
