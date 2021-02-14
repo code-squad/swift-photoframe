c4dbe6af2db6c11bdb9733134d4afe9883cb873d
Sonjh1306 <yale1306@gmail.com>
2021-02-14 17:12:55 +0900

    Complete step7 Second Scene 화면

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -11,13 +11,18 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.photoImageView.image = UIImage(named: "Demo Images/01.jpg")
+        self.photoImageView.contentMode = .scaleAspectFill
         // Do any additional setup after loading the view.
     }
     @IBOutlet weak var photoImageView: UIImageView!
     
     @IBAction func nextImageButtonTouched(_ sender: Any) {
-        self.photoImageView.image = UIImage(named: "01.jpg")
+        // 1~9까지는 앞에 0이 붙는다. 따라서 format으로 맞춰준다.
+        let randomNumber = String(format: "%02d" ,Int.random(in: 1...22))
+        let image = "Demo Images/\(randomNumber).jpg"
+        self.photoImageView.image = UIImage(named: image)
+        self.photoImageView.contentMode = .scaleAspectFill
     }
     
 
