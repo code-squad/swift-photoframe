728fe171c6298f08e03bee45e9595345f7504758
jhpark-steve <park9368@naver.com>
2021-02-15 08:19:42 +0900

    photopicker를 통하여 사진 가져오기 추가
    photopicker를 통하여 사진 가져오는 함수 추가 했습니다. 그런데 따로 권환 요청을 하는 거 같진 않았습니다. iOS 14 이후 버전에서는 다른 방법을 사용하는 것 같던데 좀 더 찾아봐야겠습니다.

--- PhotoFrame/PhotoFrame/Tab2ViewController.swift
+++ PhotoFrame/PhotoFrame/Tab2ViewController.swift
@@ -8,20 +8,37 @@
 import UIKit
 
 class Tab2ViewController: UIViewController {
-    
+    let picker = UIImagePickerController()
     @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        picker.delegate = self
         // Do any additional setup after loading the view.
     }
     
     @IBAction func nextImageButtonTouched(_ sender: Any) {
-        let url : String = "/Users/parkjungha/Documents/Xcode_Project/swift-photoframe/PhotoFrame/PhotoFrame/Photo/\(getRandomNumber()).jpg" //contentsOfFile:
+        //        let url : String = "/Users/parkjungha/Documents/Xcode_Project/swift-photoframe/PhotoFrame/PhotoFrame/Photo/\(getRandomNumber()).jpg" //contentsOfFile:
         let url2 : String = "\(getRandomNumber()).jpg"
         
         self.photoImageView.image = UIImage(named: url2)
+    }
+    
+    func openLibrary(){
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false, completion: nil)
+    }
+    
+    @IBAction func selectButtonTouched(_ sender: Any) {
+        let alert = UIAlertController(title: "사진 선택", message: "원하는 사진 선택", preferredStyle: .actionSheet)
+        let library =  UIAlertAction(title: "사진앨범", style: .default) { (action) in self.openLibrary()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(library)
+        alert.addAction(cancel)
+    
+        present(alert, animated: true, completion: nil)
     }
     
     func getRandomNumber() -> String {
@@ -36,13 +53,25 @@ class Tab2ViewController: UIViewController {
     }
     
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension Tab2ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            photoImageView.image = image
+            print(info)
+        }
+        dismiss(animated: true, completion: nil)
     }
-    */
-
 }
