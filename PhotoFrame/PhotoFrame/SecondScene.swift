d9150b4cac46eccddb16b0f4b0f23a79777027aa
Sonjh1306 <yale1306@gmail.com>
2021-02-14 16:18:51 +0900

    SecondScene 생성 및 기본 화면 구성

---
+++ PhotoFrame/PhotoFrame/SecondScene.swift
@@ -0,0 +1,34 @@
+//
+//  SecondScene.swift
+//  PhotoFrame
+//
+//  Created by sonjuhyeong on 2021/02/14.
+//
+
+import UIKit
+
+class SecondScene: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
