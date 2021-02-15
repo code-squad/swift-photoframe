b213370e2172d04d142660a271fde7f79397acc3
dahun-lee-daji <ssss6596@gmail.com>
2021-02-15 21:41:31 +0900

    step 7
    DS_Store ignore 다시 추가.
    요구사항에 따른 secondScene 내부 imageview 추가, 요구현내용 구현 완료.
    이미지 리소스 추가

--- PhotoFrame/PhotoFrame/SecondSceneViewController.swift
+++ PhotoFrame/PhotoFrame/SecondSceneViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class SecondSceneViewController: UIViewController {
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+        self.photoImageView.image = UIImage(named: "\(randomNumberAsString()).jpg")
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
         // Do any additional setup after loading the view.
+    }
+
+    func randomNumberAsString() -> String {
+        let num = Int.random(in: 1...22)
+        var sol = String()
+        if num < 10 {
+            sol = "0\(num)"
+        }
+        else {
+            sol = String(num)
+        }
+        return sol
     }
 }
