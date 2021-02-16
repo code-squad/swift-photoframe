6ea722180d74bfb1aa433e4f75f3b987ac73c423
dahun-lee-daji <ssss6596@gmail.com>
2021-02-16 14:39:37 +0900

    앨범 내 사진 선택 기능 구현
    이전에 학습했던 코드를 가져와 재사용.
    다만 아직 delegate에 대한 이해가 부족하여 이해에 빠진 고리가 존재.

--- PhotoFrame/PhotoFrame/SecondSceneViewController.swift
+++ PhotoFrame/PhotoFrame/SecondSceneViewController.swift
@@ -8,16 +8,23 @@
 import UIKit
 
 class SecondSceneViewController: UIViewController {
-    @IBOutlet weak var photoImageView: UIImageView!
     
+    let imagePicker = UIImagePickerController()
+    @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var photoFrame: UIImageView!
+    
     @IBAction func nextImageButtonTouched(_ sender: Any) {
         self.photoImageView.image = UIImage(named: "\(randomNumberAsString()).jpg")
         
     }
     
+    @IBAction func selectButtonTouched(_ sender: Any) {
+        openAlbum()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         print(#file, #line, #function, #column)
         // Do any additional setup after loading the view.
     }
@@ -33,4 +40,23 @@ class SecondSceneViewController: UIViewController {
         }
         return sol
     }
+    func openAlbum() {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: false, completion: nil)
+    }
+}
+
+extension SecondSceneViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            print("이미지 픽킹 완료")
+            
+            if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerOriginalImage")] as? UIImage {
+                photoImageView.image = image
+            }
+            
+            dismiss(animated: true, completion: nil)
+    }
+    
 }
