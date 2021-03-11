6a8ffcb2ff914a3d504a19f95c229164d1793881
JinseoPark <rarebook92@naver.com>
2021-03-11 16:39:53 +0900

    Update PhotoView Img and Create UIImagePickerController
    Use UIImagePickerController and modify function by extension, users can change image from their photo library. To do it, need to add attribute in info.plist.

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -7,47 +7,53 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController{
 
     @IBOutlet weak var photoImgView: UIImageView!
+    
+    /*사진을 불러오기 위한 컨트롤러*/
+    let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         self.photoImgView.image = UIImage(named: "01.jpg")
         self.photoImgView.contentMode = .scaleToFill
         
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+                
     }
     
     @IBAction func changeImgBtnTouched(_ sender: Any) {
+        
         let randInt = Int.random(in: 1...22)
         
-        //Path가 Stirng이기에 10 이하 숫자(n)는 0n.jpg 이며 정확한 str를 위해 if-else 구문 사용
-        if randInt < 10 {
-            self.photoImgView.image =
-                UIImage(named: "0"+String(randInt)+".jpg")
-        }else {
-            self.photoImgView.image =
-                UIImage(named: String(randInt)+".jpg")
-        }
-        
+        /*String.format을 이용해서 경로를 적음*/
+        self.photoImgView.image = UIImage(named: String(format: "%02d", randInt))
 
         /* contentMode의 scaleToFill을 이용해서 사이즈를 조정한다.*/
         self.photoImgView.contentMode = .scaleToFill
-        
 
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func selectBtnTouched(_ sender: Any) {
+        /*photoLibrary 를 통해 image를 픽할수 있고, present를 이용해 뷰 전환*/
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
-    */
+    
+    
+}
 
+/*이 작업을 하는 이유는 앨범에 들어가서 이미지를 선택 후, 다시 SecondView로 오기위한 작업
+ 이미지를 바꿔주고, 앨범 view를 dismiss 해주어야 한다.*/
+extension SecondViewController : UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.photoImgView.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
